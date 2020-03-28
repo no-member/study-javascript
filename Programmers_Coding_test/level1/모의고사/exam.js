d111dd35d89775a7db2e85dc320affbe581a121e
shin <cu7898@gmail.com>
2020-03-28 22:15:01 +0900

    Feat [2,1,2]을 넣었을 때 [2]을 반환

--- Programmers_Coding_test/level1/모의고사/exam.js
+++ Programmers_Coding_test/level1/모의고사/exam.js
@@ -1,6 +1,34 @@
-const solution = function (answer) {
-    return [1];
-};
+const solution = function (answers) {
+    const firstAnswer = [1, 2, 3, 4, 5];
+    let firstAnswerCount = 0;
+    const secondAnswer = [2, 1, 2, 3, 2, 4, 2, 5];
+    let secondAnswerCount = 0;
+    const thirdAnswer = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    let thirdAnswerCount = 0;
 
+    for (let i = 0; i < answers.length; i++) {
+        const answer = answers[i];
+        if (firstAnswer[i] === answer) {
+            firstAnswerCount++;
+        }
+        if (secondAnswer[i] === answer) {
+            secondAnswerCount++;
+        }
+        if (thirdAnswer[i] === answer) {
+            thirdAnswerCount++
+        }
+    }
+
+    const answerCountList = [firstAnswerCount, secondAnswerCount, thirdAnswerCount];
+    const max = Math.max(...answerCountList);
+
+    let answer = [];
+    for (let i = 0; i < answerCountList.length; i++){
+       if (answerCountList[i] === max ){
+            answer.push(i+1);
+       }
+    }
+    return answer;
+};
 
 module.exports = solution;
--- Programmers_Coding_test/level1/모의고사/exam.test.js
+++ Programmers_Coding_test/level1/모의고사/exam.test.js
@@ -1,5 +1,9 @@
 const solution = require('./exam');
 
-test('', () => {
+test('[1,2,3]을 넣었을 때 [1]을 반환', () => {
     expect(solution([1,2,3])).toEqual([1]);
+});
+
+test('[2,1,2]을 넣었을 때 [2]을 반환', () => {
+    expect(solution([2,1,2])).toEqual([2]);
 });
