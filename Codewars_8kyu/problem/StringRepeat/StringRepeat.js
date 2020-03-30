c7b2d55250b1e46cf549f06c6865407fe8420cd6
shin <cu7898@gmail.com>
2020-03-30 22:17:34 +0900

    Refactor #5 더욱 좋은 해결 방법
    자바스크립트에서 string을 반복하고 싶은 경우에는 repeat을 사용하자.

--- Codewars_8kyu/problem/StringRepeat/StringRepeat.js
+++ Codewars_8kyu/problem/StringRepeat/StringRepeat.js
@@ -1,9 +1,15 @@
+// const repeatString = function(repeat, string) {
+//     let answer = '';
+//     for (let i = 0; i < repeat; i++) {
+//        answer += string;
+//     }
+//     return answer;
+// };
+
 const repeatString = function(repeat, string) {
-    let answer = '';
-    for (let i = 0; i < repeat; i++) {
-       answer += string;
-    }
-    return answer;
+    return string.repeat(repeat);
 };
+
+
 
 module.exports = repeatString;
