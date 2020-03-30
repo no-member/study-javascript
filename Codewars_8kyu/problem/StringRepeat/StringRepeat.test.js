5298fcf6dfd81ed2a3e00492ec2ef0fd1c5baec7
shin <cu7898@gmail.com>
2020-03-30 22:13:47 +0900

    Test #5 repeatString(3,'ha')는 hahaha를 반환 테스트

--- Codewars_8kyu/problem/StringRepeat/StringRepeat.test.js
+++ Codewars_8kyu/problem/StringRepeat/StringRepeat.test.js
@@ -1,9 +1,5 @@
 const repeatString = require('./StringRepeat');
 
-test("repeatString(1,'*')는 *를 반환",() => {
-    expect(repeatString(1,'*')).toBe('*');
-});
-
 test("repeatString(2,'*')는 **를 반환",() => {
     expect(repeatString(2,'*')).toBe('**');
 });
@@ -11,3 +7,8 @@ test("repeatString(2,'*')는 **를 반환",() => {
 test("repeatString(5,'!')는 !!!!!를 반환",() => {
     expect(repeatString(5,'!')).toBe('!!!!!');
 });
+
+test("repeatString(3,'ha')는 hahaha를 반환",() => {
+    expect(repeatString(3,'ha')).toBe('hahaha');
+});
+
