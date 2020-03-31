8cc911dbfb49bf89257219457c11eee924114c3e
shin <cu7898@gmail.com>
2020-03-31 20:48:01 +0900

    Refactor #7

--- Codewars_8kyu/problem/FindTheSmallestIntegerInTheArray/FindSmallet.test.js
+++ Codewars_8kyu/problem/FindTheSmallestIntegerInTheArray/FindSmallet.test.js
@@ -1,9 +1,5 @@
 const findSmallest = require('./FindSmallest');
 
-test('1 + 1 is 2',() => {
-    expect(1 + 1).toBe(2);
-});
-
 test('findSmallest([1,2] toBe 1', ()=>{
     expect(findSmallest([1,2])).toBe(1);
 });
