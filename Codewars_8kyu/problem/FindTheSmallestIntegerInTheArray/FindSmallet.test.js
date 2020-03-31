8c13adfadcfed99e8ea4f95242e63b79a3b376e4
shin <cu7898@gmail.com>
2020-03-31 20:58:58 +0900

    TestF #7 findSmallest([34, -345, -1, 100]) toBe -345

--- Codewars_8kyu/problem/FindTheSmallestIntegerInTheArray/FindSmallet.test.js
+++ Codewars_8kyu/problem/FindTheSmallestIntegerInTheArray/FindSmallet.test.js
@@ -11,3 +11,7 @@ test('findSmallest([5, 4, 2] toBe 2', () => {
 test('findSmallest([78,56,232,12,8]) toBe 8', () => {
     expect(findSmallest([78, 56, 232, 12, 8])).toBe(8);
 });
+
+test('findSmallest([34, -345, -1, 100]) toBe -345', () => {
+    expect(findSmallest([34, -345, -1, 100])).toBe(-345);
+});
