6ab54c3f1589a72917e48ee56aa735411ac5c974
shin <cu7898@gmail.com>
2020-04-01 00:03:37 +0900

    Final #11 Codewars-Convert a Number to a String!

--- Codewars_8kyu/problem/ConvertNumberString/NumberToString.js
+++ Codewars_8kyu/problem/ConvertNumberString/NumberToString.js
@@ -1,7 +1,8 @@
-const numberToString = function(num) {
-    let answer = '';
-    answer += num;
-    return answer;
+const numberToString = function(number) {
+    // let answer = '';
+    // answer += num;
+    // return answer;
+    return number.toString();
 };
 
 module.exports = numberToString;
