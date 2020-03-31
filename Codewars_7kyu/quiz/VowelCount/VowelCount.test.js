5bf96d5dcaffce0920f32c0c60008cad26b96f4d
shin <cu7898@gmail.com>
2020-04-01 02:34:29 +0900

    TestF #13 vowelCount("abracadabra")).toBe(5)

--- Codewars_7kyu/quiz/VowelCount/VowelCount.test.js
+++ Codewars_7kyu/quiz/VowelCount/VowelCount.test.js
@@ -7,3 +7,8 @@ test('vowelCount("hi") is 1', () => {
 test('vowelCount("ahii") is 3', () => {
     expect(vowelCount("ahii")).toBe(3);
 });
+
+
+test('vowelCount("abracadabra") is 5', () => {
+    expect(vowelCount("abracadabra")).toBe(5);
+});
