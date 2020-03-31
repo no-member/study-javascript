54bd9f7a0220be9ed7d423ef3aad172367ecd904
shin <cu7898@gmail.com>
2020-04-01 02:39:53 +0900

    Refactor #13 이름의 변경

--- Codewars_7kyu/quiz/VowelCount/VowelCount.js
+++ Codewars_7kyu/quiz/VowelCount/VowelCount.js
@@ -4,14 +4,14 @@ const vowelCount = function (str) {
     let answer = 0;
     for (let i = 0; i < stringList.length; i++) {
         const char = stringList[i];
-        if (temp(char)) {
+        if (isVowel(char)) {
             answer += 1;
         }
     }
     return answer;
 };
 
-const temp = function (char) {
+const isVowel = function (char) {
     const vowel = ['a', 'e', 'i', 'o', 'u'];
     for (let i = 0; i < vowel.length; i++) {
         if (char === vowel[i]) {
