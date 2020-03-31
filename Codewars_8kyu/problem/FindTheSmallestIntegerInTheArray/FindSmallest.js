e717715c896b36f2cbd04194a349e70faebd68ec
shin <cu7898@gmail.com>
2020-03-31 21:06:32 +0900

    Final #7 더 좋은 방법

--- Codewars_8kyu/problem/FindTheSmallestIntegerInTheArray/FindSmallest.js
+++ Codewars_8kyu/problem/FindTheSmallestIntegerInTheArray/FindSmallest.js
@@ -1,8 +1,9 @@
 const findSmallest = function (array) {
-    array.sort(function (a, b) {
-        return a - b;
-    });
-    return array[0];
+    // array.sort(function (a, b) {
+    //     return a - b;
+    // });
+    // return array[0];
+    return Math.min(...array);
 };
 
 module.exports = findSmallest;
