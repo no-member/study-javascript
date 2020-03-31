9d4da00444d6ed5efa3c37e9b42bffc860b23f8c
shin <cu7898@gmail.com>
2020-03-31 23:28:52 +0900

    Test #9 boolToWord(false) is No

--- Codewars_8kyu/problem/ConvertBooleanValuesToStringsYesOrNo/BooleanToWord.test.js
+++ Codewars_8kyu/problem/ConvertBooleanValuesToStringsYesOrNo/BooleanToWord.test.js
@@ -3,3 +3,7 @@ const boolToWord = require('./BooleanToWord');
 test('boolToWord(true) is Yes', () => {
     expect(boolToWord(true)).toBe('Yes');
 });
+
+test('boolToWord(false) is No', () => {
+    expect(boolToWord(false)).toBe('No');
+});
