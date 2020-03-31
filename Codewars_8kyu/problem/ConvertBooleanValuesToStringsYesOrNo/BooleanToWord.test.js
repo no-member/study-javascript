becefe21232b23b504f5edc29232fb7e59163b71
shin <cu7898@gmail.com>
2020-03-31 23:26:16 +0900

    Refactor #9

--- Codewars_8kyu/problem/ConvertBooleanValuesToStringsYesOrNo/BooleanToWord.test.js
+++ Codewars_8kyu/problem/ConvertBooleanValuesToStringsYesOrNo/BooleanToWord.test.js
@@ -1,9 +1,5 @@
 const boolToWord = require('./BooleanToWord');
 
-test('1+1 is 2', () => {
-    expect(1 + 1).toBe(2);
-});
-
 test('boolToWord(true) is Yes', () => {
     expect(boolToWord(true)).toBe('Yes');
 });
