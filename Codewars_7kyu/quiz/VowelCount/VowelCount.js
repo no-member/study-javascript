f68d205a64e6e8dd398e844d049014285d20c54d
shin <cu7898@gmail.com>
2020-04-01 02:28:24 +0900

    Feat #13 vowelCount("ahii")).toBe(3)

--- Codewars_7kyu/quiz/VowelCount/VowelCount.js
+++ Codewars_7kyu/quiz/VowelCount/VowelCount.js
@@ -1,7 +1,21 @@
-const vowelCount = function(str) {
+const vowelCount = function (str) {
 
-    return 1;
+    const vowel = ['a', 'e', 'i', 'o', 'u'];
+    const stringList = [...str];
+
+    let answer = 0;
+
+    for (let i = 0; i < stringList.length; i++) {
+        const char = stringList[i];
+        for (let j = 0; j < vowel.length; j++) {
+            if (char === vowel[j]) {
+                answer += 1;
+                break;
+            }
+        }
+    }
+
+    return answer;
 };
-
 
 module.exports = vowelCount;
