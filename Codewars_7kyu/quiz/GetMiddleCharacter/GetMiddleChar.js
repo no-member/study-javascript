0f10beb13e82e48d12e9872a715e790d2c5d68f7
shin <cu7898@gmail.com>
2020-04-01 14:10:05 +0900

    Final #15 더 좋은 방법으로 최종 정리

--- Codewars_7kyu/quiz/GetMiddleCharacter/GetMiddleChar.js
+++ Codewars_7kyu/quiz/GetMiddleCharacter/GetMiddleChar.js
@@ -1,15 +1,17 @@
 const getMiddle = function (str) {
-    const length = str.length;
-    const halfLength = length / 2;
-    let answer;
+    // const length = str.length;
+    // const halfLength = length / 2;
+    // let answer;
+    //
+    // if (length % 2 === 1) {
+    //     answer = str.slice(halfLength, halfLength + 1);
+    //     return answer;
+    // }
+    //
+    // answer = str.slice(halfLength - 1, halfLength + 1);
+    // return answer;
 
-    if (length % 2 === 1) {
-        answer = str.slice(halfLength, halfLength + 1);
-        return answer;
-    }
-
-    answer = str.slice(halfLength - 1, halfLength + 1);
-    return answer;
+    return str.slice((str.length - 1) / 2, str.length / 2 + 1);
 };
 
 module.exports = getMiddle;
