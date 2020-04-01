a69c6e8ce08016cd0747f7952047b5bf79db2862
shin <cu7898@gmail.com>
2020-04-01 13:38:46 +0900

    Feat #15 getMiddle("HoH")는 "o"를 반환

--- Codewars_7kyu/quiz/GetMiddleCharacter/GetMiddleChar.js
+++ Codewars_7kyu/quiz/GetMiddleCharacter/GetMiddleChar.js
@@ -1,4 +1,9 @@
-const getMiddle = function(str) {
+const getMiddle = function (str) {
+    const length = str.length;
+    if (length % 2 === 1) {
+        str = str[Math.floor(length / 2)];
+        return str;
+    }
     return str;
 };
 
