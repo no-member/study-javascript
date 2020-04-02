e689a282218d9bfcb1659a93c1cac54563781012
shin <cu7898@gmail.com>
2020-04-02 22:24:00 +0900

    Feat #21 accum("Cb")는 "CBb"를 반환한다.

--- Codewars_7kyu/quiz/Mumbling/Accum.js
+++ Codewars_7kyu/quiz/Mumbling/Accum.js
@@ -1,5 +1,19 @@
 const accum = function(str) {
-    str.toUpperCase();
+    let answer = '';
+    let charList = [...str];
+    for (let i = 0; i < str.length; i++) {
+        for(let j = 0; j < i + 1 ; j++) {
+            if (j === 0) {
+                answer += charList[i].toUpperCase();
+                continue;
+            }
+            answer += charList[i].toLowerCase();
+        }
+        if (i !== str.length -1) {
+            answer += '-';
+        }
+    }
+    return answer;
 };
 
 module.exports = accum;
--- Codewars_7kyu/quiz/Mumbling/Accum.test.js
+++ Codewars_7kyu/quiz/Mumbling/Accum.test.js
@@ -4,4 +4,7 @@ test('accum("a")는 "A"를 반환한다.', ()=>{
     expect(accum('a')).toBe('A');
 });
 
+test('accum("Cb")는 "CBb"를 반환한다.', ()=>{
+    expect(accum('Cb')).toBe('C-Bb');
+});
 
