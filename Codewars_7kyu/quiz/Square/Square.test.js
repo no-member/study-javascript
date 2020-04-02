cbf53ed5a08f6f6ec5082439032ddf242f09225a
shin <cu7898@gmail.com>
2020-04-02 11:35:43 +0900

    Test #19 isSquare(4)은 true를 반환함

--- Codewars_7kyu/quiz/Square/Square.test.js
+++ Codewars_7kyu/quiz/Square/Square.test.js
@@ -7,3 +7,7 @@ test('isSquare(1)은 true를 반환함',()=>{
 test('isSquare(2)은 false를 반환함',()=>{
     expect(isSquare(2)).toBe(false);
 });
+
+test('isSquare(4)은 true를 반환함',()=>{
+    expect(isSquare(4)).toBe(true);
+});
