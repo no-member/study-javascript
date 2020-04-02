398cf0be2dfd7cb4f7bfa9b57c270ad5ffa374f4
shin <cu7898@gmail.com>
2020-04-02 22:28:42 +0900

    TestF #21 accum("cwAt")는 "C-Ww-Aaa-Tttt"를 반환한다.

--- Codewars_7kyu/quiz/Mumbling/Accum.test.js
+++ Codewars_7kyu/quiz/Mumbling/Accum.test.js
@@ -11,3 +11,8 @@ test('accum("Cb")는 "CBb"를 반환한다.', ()=>{
 test('accum("abcd")는 "A-Bb-Ccc-Dddd"를 반환한다.', ()=>{
     expect(accum('abcd')).toBe('A-Bb-Ccc-Dddd');
 });
+
+test('accum("cwAt")는 "C-Ww-Aaa-Tttt"를 반환한다.', ()=>{
+    expect(accum('cwAt')).toBe('C-Ww-Aaa-Tttt');
+});
+
