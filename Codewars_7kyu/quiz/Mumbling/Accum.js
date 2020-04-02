58b19cdbeecb613f4881db04478d13b739eb37b4
shin <cu7898@gmail.com>
2020-04-02 21:02:22 +0900

    Test #21 accum("a")는 "A"를 반환한다.

---
+++ Codewars_7kyu/quiz/Mumbling/Accum.js
@@ -0,0 +1,5 @@
+const accum = function() {
+
+};
+
+module.exports = accum;
---
+++ Codewars_7kyu/quiz/Mumbling/Accum.test.js
@@ -0,0 +1,7 @@
+const accum = require('./Accum');
+
+test('a는 A를 반환한다.', ()=>{
+    expect(accum('a')).toBe('A');
+});
+
+
