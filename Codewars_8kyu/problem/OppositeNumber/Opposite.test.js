6a648c5fc1a42b2f081e325ee7e66d0270e06c60
shin <cu7898@gmail.com>
2020-04-04 10:23:32 +0900

    Test #25 opposite(3)은 -3를 반환함

--- Codewars_8kyu/problem/OppositeNumber/Opposite.test.js
+++ Codewars_8kyu/problem/OppositeNumber/Opposite.test.js
@@ -3,3 +3,7 @@ const opposite = require('./Opposite');
 test('opposite(1)은 -1를 반환함',() =>{
     expect(opposite(1)).toBe(-1);
 });
+
+test('opposite(3)은 -3를 반환함',() =>{
+    expect(opposite(3)).toBe(-3);
+});
