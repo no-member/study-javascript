171273acd78f716152adde282f9a4b5c7cf91b0b
shin <cu7898@gmail.com>
2020-04-06 02:12:25 +0900

    Final #27 마지막 정리

--- Codewars_8kyu/quiz/RemoveFirstLast/RemoveChar.js
+++ Codewars_8kyu/quiz/RemoveFirstLast/RemoveChar.js
@@ -1,5 +1,6 @@
-const removeChar = function (str) {
-    return str.slice(1, str.length - 1);
+const removeChar = str => {
+    // return str.slice(1, str.length - 1);
+    return str.slice(1, -1)
 };
 
 module.exports = removeChar;
