dd2fbc5ad9fde57e95da83b8aaf76443c7aea896
shin <cu7898@gmail.com>
2020-04-06 02:09:05 +0900

    Test #27 remove("place")은 "lac"을 반환한다.

--- Codewars_8kyu/quiz/RemoveFirstLast/RemoveChar.test.js
+++ Codewars_8kyu/quiz/RemoveFirstLast/RemoveChar.test.js
@@ -3,3 +3,7 @@ const removeChar = require('./RemoveChar');
 test('remove("person")은 "erso"을 반환한다.',() =>{
     expect(removeChar('person')).toBe('erso');
 });
+
+test('remove("place")은 "lac"을 반환한다.',() =>{
+    expect(removeChar('place')).toBe('lac');
+});
