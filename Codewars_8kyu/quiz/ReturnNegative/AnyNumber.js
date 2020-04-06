78953f142fdb8bb4c65fbbe900eb58128d4cea99
shin <cu7898@gmail.com>
2020-04-07 08:06:11 +0900

    Refactor #27

--- Codewars_8kyu/quiz/ReturnNegative/AnyNumber.js
+++ Codewars_8kyu/quiz/ReturnNegative/AnyNumber.js
@@ -1,8 +1,5 @@
 const makeNegative = function (number) {
-    if (number < 0 ) {
-        return number;
-    }
-    return -number;
+    return number < 0 ? number : -number;
 };
 
 module.exports = makeNegative;
