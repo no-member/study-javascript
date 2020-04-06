7d8fa53203e603d112fe7423ab5a56cde48dd17a
shin <cu7898@gmail.com>
2020-04-07 08:03:31 +0900

    TestF #27 makeNegative(123)은 -123를 반환함

--- Codewars_8kyu/quiz/ReturnNegative/AnyNumber.test.js
+++ Codewars_8kyu/quiz/ReturnNegative/AnyNumber.test.js
@@ -3,3 +3,7 @@ const makeNegative = require('./AnyNumber');
 test('makeNegative(1)은 -1를 반환함',() =>{
     expect(makeNegative(1)).toBe(-1);
 });
+
+test('makeNegative(123)은 -123를 반환함',() =>{
+    expect(makeNegative(123)).toBe(-123);
+});
