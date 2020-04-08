9dde5029c0dfd9287763e81914ec1aa88ec4a33e
shin <cu7898@gmail.com>
2020-04-08 11:20:22 +0900

    Test #29 removeSpaces("Hi man")은 "Himan"를 반환함

--- Codewars_8kyu/quiz/RemoveSpaces/RemoveSpaces.test.js
+++ Codewars_8kyu/quiz/RemoveSpaces/RemoveSpaces.test.js
@@ -3,3 +3,7 @@ const removeSpaces = require('./RemoveSpaces');
 test('removeSpaces(" Hi ")은 "Hi"를 반환함', () => {
     expect(removeSpaces(" Hi ")).toBe("Hi");
 });
+
+test('removeSpaces("Hi man")은 "Himan"를 반환함', () => {
+    expect(removeSpaces("Hi man")).toBe("Himan");
+});
