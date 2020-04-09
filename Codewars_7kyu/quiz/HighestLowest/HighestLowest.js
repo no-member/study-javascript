fd63097b9e900b2bfd18b549f592053ac8914751
shin <cu7898@gmail.com>
2020-04-10 01:13:06 +0900

    Refacotr #31 필요 없는 것 제거

--- Codewars_7kyu/quiz/HighestLowest/HighestLowest.js
+++ Codewars_7kyu/quiz/HighestLowest/HighestLowest.js
@@ -1,5 +1,4 @@
 const find = function(numbers) {
-    console.log(numbers.split(" ").sort((a, b) => a - b));
     const numbersList = numbers.split(" ").sort((a, b) => a - b);
     return `${numbersList[numbersList.length - 1]} ${numbersList[0]}`;
 };
