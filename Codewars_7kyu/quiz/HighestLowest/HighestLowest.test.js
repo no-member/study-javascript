6bbf35f719d76280c182194d8a7a512eaa9e466f
shin <cu7898@gmail.com>
2020-04-10 01:09:52 +0900

    Refactor #31

--- Codewars_7kyu/quiz/HighestLowest/HighestLowest.test.js
+++ Codewars_7kyu/quiz/HighestLowest/HighestLowest.test.js
@@ -1,9 +1,5 @@
 const find = require('./HighestLowest');
 
-test("find('1 2 3')은 '3 1'을 반환한다.", () => {
-    expect(find('1 2 3')).toBe('3 1')
-});
-
 test("find('3 2 1 4')은 '4 1'을 반환한다.", () => {
     expect(find('3 2 1 4')).toBe('4 1')
 });
