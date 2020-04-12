d82d388bc7cfdb71aaa64d88ecbaefae1705a55a
shin <cu7898@gmail.com>
2020-04-12 11:28:02 +0900

    Feat #33 changeNumber(14)는 116를 반환함

--- Codewars_7kyu/quiz/SquareEveryDigit/SquareDigit.js
+++ Codewars_7kyu/quiz/SquareEveryDigit/SquareDigit.js
@@ -1,5 +1,13 @@
 const changeNumber = function (number) {
-    return Math.pow(number, 2);
+    let answer = "";
+
+    while (number > 0) {
+        const digitNumber = number % 10;
+        answer = Math.pow(digitNumber, 2) + answer;
+        number = parseInt(number / 10);
+    }
+
+    return parseInt(answer);
 };
 
 module.exports = changeNumber;
