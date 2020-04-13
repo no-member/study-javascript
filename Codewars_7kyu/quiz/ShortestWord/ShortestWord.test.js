ef19968a8c2ecd22d74dfae6e4c08b675c2f5367
shin <cu7898@gmail.com>
2020-04-13 23:32:44 +0900

    TestF #35 countShortest("my good plan")은 2를 반환함

--- Codewars_7kyu/quiz/ShortestWord/ShortestWord.test.js
+++ Codewars_7kyu/quiz/ShortestWord/ShortestWord.test.js
@@ -7,3 +7,7 @@ test('countShortest("hi")은 2를 반환함',()=>{
 test('countShortest("Hello friend")은 5를 반환함',()=>{
     expect(countShortest('Hello friend')).toBe(5);
 });
+
+test('countShortest("my good plan")은 2를 반환함',()=>{
+    expect(countShortest('my good plan')).toBe(2);
+});
