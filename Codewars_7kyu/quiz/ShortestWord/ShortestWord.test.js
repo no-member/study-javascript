ba90cc94f9e18435c53b69e2180e069c6afebb3d
shin <cu7898@gmail.com>
2020-04-13 23:34:35 +0900

    TestF #35 countShortest("I am good I am okay")은 1를 반환함

--- Codewars_7kyu/quiz/ShortestWord/ShortestWord.test.js
+++ Codewars_7kyu/quiz/ShortestWord/ShortestWord.test.js
@@ -11,3 +11,7 @@ test('countShortest("Hello friend")은 5를 반환함',()=>{
 test('countShortest("my good plan")은 2를 반환함',()=>{
     expect(countShortest('my good plan')).toBe(2);
 });
+
+test('countShortest("I am good I am okay")은 1를 반환함',()=>{
+    expect(countShortest('I am good I am okay')).toBe(1);
+});
