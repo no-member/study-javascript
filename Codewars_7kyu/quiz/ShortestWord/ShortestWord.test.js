20faaec04ba5e59fda648653f6c5341f2bbc7798
shin <cu7898@gmail.com>
2020-04-13 22:16:18 +0900

    Test #35 countShortest("Hello friend")은 5를 반환함

--- Codewars_7kyu/quiz/ShortestWord/ShortestWord.test.js
+++ Codewars_7kyu/quiz/ShortestWord/ShortestWord.test.js
@@ -4,3 +4,6 @@ test('countShortest("hi")은 2를 반환함',()=>{
     expect(countShortest('hi')).toBe(2);
 });
 
+test('countShortest("Hello friend")은 5를 반환함',()=>{
+    expect(countShortest('Hello friend')).toBe(5);
+});
