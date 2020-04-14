1431900c0a5513ada6d70c595a49b90d8f1c0947
shin <cu7898@gmail.com>
2020-04-14 21:38:52 +0900

    TestF #37 xo('XoaaxdddOOee')는 false를 반환한다

--- Codewars_7kyu/quiz/ExesOhs/XO.test.js
+++ Codewars_7kyu/quiz/ExesOhs/XO.test.js
@@ -15,3 +15,7 @@ test("xo('xoxo')는 true를 반환한다.", () => {
 test("xo('Xoxo')는 true를 반환한다.", () => {
     expect(xo('Xoxo')).toBe(true);
 });
+
+test("xo('XoaaxdddOOee')는 false를 반환한다.", () => {
+    expect(xo('XoaaxdddOOee')).toBe(false);
+});
