81f83a8ee58129a2f994974daa01323632b126f1
shin <cu7898@gmail.com>
2020-04-14 21:33:33 +0900

    Feat #37 xo('xxo')는 false를 반환한다.

--- Codewars_7kyu/quiz/ExesOhs/XO.js
+++ Codewars_7kyu/quiz/ExesOhs/XO.js
@@ -1,5 +1,8 @@
 const xo = function (str) {
-    return true
+    const removeX = str.replace(/x/g, '');
+    const removeO = str.replace(/o/g, '');
+
+    return removeX.length === removeO.length;
 };
 
 module.exports = xo;
