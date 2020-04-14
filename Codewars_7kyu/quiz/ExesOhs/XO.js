771aac148272067f0c42deaa9dcc5d0ea32736c4
shin <cu7898@gmail.com>
2020-04-14 21:36:41 +0900

    Feat #37 xo('Xoxo')는 true를 반환한다.

--- Codewars_7kyu/quiz/ExesOhs/XO.js
+++ Codewars_7kyu/quiz/ExesOhs/XO.js
@@ -1,6 +1,6 @@
 const xo = function (str) {
-    const removeX = str.replace(/x/g, '');
-    const removeO = str.replace(/o/g, '');
+    const removeX = str.replace(/x/gi, '');
+    const removeO = str.replace(/o/gi, '');
 
     return removeX.length === removeO.length;
 };
