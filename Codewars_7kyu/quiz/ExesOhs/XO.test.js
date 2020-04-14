6b05b2a25d9890e5f21fdd521d0595712df483f7
shin <cu7898@gmail.com>
2020-04-14 21:23:34 +0900

    Test #37 xo('xxo')는 false를 반환한다.

--- Codewars_7kyu/quiz/ExesOhs/XO.test.js
+++ Codewars_7kyu/quiz/ExesOhs/XO.test.js
@@ -3,3 +3,7 @@ const xo = require('./XO');
 test("xo('xo')는 true를 반환한다.", () => {
     expect(xo('xo')).toBe(true);
 });
+
+test("xo('xxo')는 false를 반환한다.", () => {
+    expect(xo('xxo')).toBe(false);
+});
