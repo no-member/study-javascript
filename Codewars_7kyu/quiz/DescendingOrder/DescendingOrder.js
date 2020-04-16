250167df46494843b5298db7559a5ca818cd7f5d
shin <cu7898@gmail.com>
2020-04-17 04:18:32 +0900

    Refactor #39

--- Codewars_7kyu/quiz/DescendingOrder/DescendingOrder.js
+++ Codewars_7kyu/quiz/DescendingOrder/DescendingOrder.js
@@ -1,8 +1,8 @@
 const descendingOrder = function (number) {
-    const numberList = number.toString().split("").sort(function (a, b) {
+    const descendingStringNumber = number.toString().split('').sort(function (a, b) {
         return b - a;
-    }).join("");
-    return Number(numberList);
+    }).join('');
+    return Number(descendingStringNumber);
 };
 
 module.exports = descendingOrder;
