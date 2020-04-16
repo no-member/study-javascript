a68b3b1288ceff2e1f1f542dbcc6fd67c8d0570a
shin <cu7898@gmail.com>
2020-04-17 03:56:06 +0900

    Test #39 descendingOrder(14)은 14을 반환

--- Codewars_7kyu/quiz/DescendingOrder/DescendingOrder.test.js
+++ Codewars_7kyu/quiz/DescendingOrder/DescendingOrder.test.js
@@ -3,3 +3,7 @@ const descendingOrder = require('./DescendingOrder');
 test('descendingOrder(1)은 1을 반환함', ()=>{
     expect(descendingOrder(1)).toBe(1);
 });
+
+test('descendingOrder(14)은 41을 반환함', ()=>{
+    expect(descendingOrder(14)).toBe(41);
+});
