5690690971e392bb4356d305c1628e303006ad0c
shin <cu7898@gmail.com>
2020-04-19 01:39:23 +0900

    Test #43 jadenCasting("i have")는 "I Have"를 반환한다.

--- Codewars_7kyu/quiz/JadenCasingStrings/JadenCasting.test.js
+++ Codewars_7kyu/quiz/JadenCasingStrings/JadenCasting.test.js
@@ -3,3 +3,7 @@ const jadenCasting = require('./JadenCasting');
 test('jadenCasting("must")는 "Must"를 반환한다.',()=>{
     expect(jadenCasting('must')).toBe('Must');
 });
+
+test('jadenCasting("i have")는 "I Have"를 반환한다.',()=>{
+    expect(jadenCasting('i have')).toBe('I Have');
+});
