95161693925c78da27d9f0635386bf91fa7fa654
shin <cu7898@gmail.com>
2020-04-19 01:44:28 +0900

    TestF #43 jadenCasting("ma ma maaa?")는 "Ma Ma Maaa?"를 반환한다.

--- Codewars_7kyu/quiz/JadenCasingStrings/JadenCasting.test.js
+++ Codewars_7kyu/quiz/JadenCasingStrings/JadenCasting.test.js
@@ -7,3 +7,7 @@ test('jadenCasting("must")는 "Must"를 반환한다.',()=>{
 test('jadenCasting("i have")는 "I Have"를 반환한다.',()=>{
     expect(jadenCasting('i have')).toBe('I Have');
 });
+
+test('jadenCasting("ma ma maaa?")는 "Ma Ma Maaa?"를 반환한다.',()=>{
+    expect(jadenCasting('ma ma maaa?')).toBe('Ma Ma Maaa?');
+});
