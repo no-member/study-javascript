bd3b059368d65a21e090f1a4b976945336eba3eb
shin <cu7898@gmail.com>
2020-04-20 21:52:40 +0900

    TestF #45 getSum(-2, 3)은 3을 반환한다.

--- Codewars_7kyu/quiz/SumNumbers /SumNumbers.test.js
+++ Codewars_7kyu/quiz/SumNumbers /SumNumbers.test.js
@@ -7,3 +7,7 @@ test('getSum(1, 0)은 1을 반환한다.', () => {
 test('getSum(1, 3)은 6을 반환한다.', () => {
     expect(getSum(1, 3)).toBe(6);
 });
+
+test('getSum(-2, 3)은 3을 반환한다.', () => {
+    expect(getSum(-2, 3)).toBe(3);
+});
