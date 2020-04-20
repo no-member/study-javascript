31eb8edd151de6cbd0571acfc5a420a6729422f6
shin <cu7898@gmail.com>
2020-04-20 21:51:35 +0900

    TestF #45 getSum(1, 3)은 6을 반환한다.

--- Codewars_7kyu/quiz/SumNumbers /SumNumbers.test.js
+++ Codewars_7kyu/quiz/SumNumbers /SumNumbers.test.js
@@ -3,3 +3,7 @@ const getSum = require('./SumNumbers');
 test('getSum(1, 0)은 1을 반환한다.', () => {
     expect(getSum(1, 0)).toBe(1);
 });
+
+test('getSum(1, 3)은 6을 반환한다.', () => {
+    expect(getSum(1, 3)).toBe(6);
+});
