089325358b6c61ab0738bf95f44cc052ca7c9a4c
shin <cu7898@gmail.com>
2020-04-20 21:57:35 +0900

    Final #45 최종정리

--- Codewars_7kyu/quiz/SumNumbers /SumNumbers.js
+++ Codewars_7kyu/quiz/SumNumbers /SumNumbers.js
@@ -1,5 +1,10 @@
 const getSum = function (num1, num2) {
-    return Math.floor((num1 + num2) * (Math.abs(num1 - num2) + 1) / 2);
+
+    return (num1 + num2) * (Math.abs(num1 - num2) + 1) / 2;
+
+    // const min = Math.min(num1, num2);
+    // const max = Math.max(num1, num2);
+    // return (max - min + 1) * (min + max) / 2;
 };
 
 module.exports = getSum;
