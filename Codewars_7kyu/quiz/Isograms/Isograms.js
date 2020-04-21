c2a9e3913566dd4ecfe236e81aef3c5727989c89
shin <cu7898@gmail.com>
2020-04-21 09:15:58 +0900

    Test #45 test 케이스 추가

--- Codewars_7kyu/quiz/Isograms/Isograms.js
+++ Codewars_7kyu/quiz/Isograms/Isograms.js
@@ -1,6 +1,6 @@
 const isIsogram = function (word) {
     const charList = word.split('');
-    
+
     return Array.from(new Set(charList)).length === charList.length;
 };
 
--- Codewars_7kyu/quiz/Isograms/Isograms.test.js
+++ Codewars_7kyu/quiz/Isograms/Isograms.test.js
@@ -4,6 +4,14 @@ test('isIsogram("hi")는 true를 반환한다.', ()=>{
     expect(isIsogram('hi')).toBe(true);
 });
 
+test('isIsogram("isogram")는 true를 반환한다.', ()=>{
+    expect(isIsogram('isogram')).toBe(true);
+});
+
 test('isIsogram("moose")는 false를 반환한다.', ()=>{
     expect(isIsogram('moose')).toBe(false);
+});
+
+test('isIsogram("sense")는 false를 반환한다.', ()=>{
+    expect(isIsogram('sense')).toBe(false);
 });
