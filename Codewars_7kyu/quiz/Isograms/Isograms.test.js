b67b811de241bd80e78a4480d7c7db732bec0d38
shin <cu7898@gmail.com>
2020-04-21 09:18:51 +0900

    Test #45 테스트 케이스의 추가

--- Codewars_7kyu/quiz/Isograms/Isograms.test.js
+++ Codewars_7kyu/quiz/Isograms/Isograms.test.js
@@ -4,6 +4,10 @@ test('isIsogram("hi")는 true를 반환한다.', ()=>{
     expect(isIsogram('hi')).toBe(true);
 });
 
+test('isIsogram("hole")는 true를 반환한다.', ()=>{
+    expect(isIsogram('hole')).toBe(true);
+});
+
 test('isIsogram("isogram")는 true를 반환한다.', ()=>{
     expect(isIsogram('isogram')).toBe(true);
 });
@@ -18,4 +22,8 @@ test('isIsogram("moOse")는 false를 반환한다.', ()=>{
 
 test('isIsogram("sense")는 false를 반환한다.', ()=>{
     expect(isIsogram('sense')).toBe(false);
+});
+
+test('isIsogram("senSe")는 false를 반환한다.', ()=>{
+    expect(isIsogram('senSe')).toBe(false);
 });
