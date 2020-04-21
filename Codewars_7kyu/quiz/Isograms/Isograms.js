29c6dfedad4571aaef295c65577dca587d4fdd9a
shin <cu7898@gmail.com>
2020-04-21 09:22:24 +0900

    Final #45 최종 정리

--- Codewars_7kyu/quiz/Isograms/Isograms.js
+++ Codewars_7kyu/quiz/Isograms/Isograms.js
@@ -1,7 +1,8 @@
 const isIsogram = function (word) {
-    const charList = word.toLowerCase().split('');
+    // const charList = word.toLowerCase().split('');
+    // return Array.from(new Set(charList)).length === charList.length;
 
-    return Array.from(new Set(charList)).length === charList.length;
+    return new Set(word.toUpperCase()).size === word.length;
 };
 
 module.exports = isIsogram;
