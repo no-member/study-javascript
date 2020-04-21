dc2d33ceeea757a28d0bc07fc853b13caaf64ea8
shin <cu7898@gmail.com>
2020-04-21 09:10:26 +0900

    Test #45 isIsogram("moose")는 false를 반환한다.

--- Codewars_7kyu/quiz/Isograms/Isograms.test.js
+++ Codewars_7kyu/quiz/Isograms/Isograms.test.js
@@ -4,3 +4,6 @@ test('isIsogram("hi")는 true를 반환한다.', ()=>{
     expect(isIsogram('hi')).toBe(true);
 });
 
+test('isIsogram("moose")는 false를 반환한다.', ()=>{
+    expect(isIsogram('moose')).toBe(false);
+});
