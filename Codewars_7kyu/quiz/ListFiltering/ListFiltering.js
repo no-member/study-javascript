1f7044f3bd7b7a5349c69e6dcf9e7cee35fdac4a
shin <cu7898@gmail.com>
2020-04-22 21:59:49 +0900

    Feat #49 listNumberFilter([1,2])는 [1, 2]를 반환한다.

--- Codewars_7kyu/quiz/ListFiltering/ListFiltering.js
+++ Codewars_7kyu/quiz/ListFiltering/ListFiltering.js
@@ -1,5 +1,9 @@
 const listNumberFilter = function (list) {
-    return list;
+    return list.filter(function (element) {
+        if (typeof element === 'number'){
+            return element
+        }
+    });
 };
 
 module.exports = listNumberFilter;
--- Codewars_7kyu/quiz/ListFiltering/ListFiltering.test.js
+++ Codewars_7kyu/quiz/ListFiltering/ListFiltering.test.js
@@ -4,3 +4,6 @@ test('listNumberFilter([1,2])는 [1, 2]를 반환한다.', () => {
     expect(listNumberFilter([1, 2])).toStrictEqual([1, 2]);
 });
 
+test('listNumberFilter([1,2, "a"])는 [1, 2]를 반환한다.', () => {
+    expect(listNumberFilter([1, 2, "a"])).toStrictEqual([1, 2]);
+});
