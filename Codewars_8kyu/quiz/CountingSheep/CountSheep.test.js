5daadaf3f9a471922dd5b87049fa75643c141a1c
shin <cu7898@gmail.com>
2020-04-24 02:04:13 +0900

    Test #55 테스트 케이스 추가

--- Codewars_8kyu/quiz/CountingSheep/CountSheep.test.js
+++ Codewars_8kyu/quiz/CountingSheep/CountSheep.test.js
@@ -4,3 +4,13 @@ test('countSheep(list)은 2를 반환함',() =>{
     const list = [true, true, false ];
     expect(countSheep(list)).toBe(2);
 });
+
+test('countSheep(list)은 3를 반환함',() =>{
+    const list = [true, true, false, false, true ];
+    expect(countSheep(list)).toBe(3);
+});
+
+test('countSheep(list)은 1를 반환함',() =>{
+    const list = [false, true, false, false ];
+    expect(countSheep(list)).toBe(1);
+});
