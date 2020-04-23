2fd61f11fa4a253fc3d653e1bc19f10483af1d46
shin <cu7898@gmail.com>
2020-04-24 02:27:32 +0900

    Final #55 마지막 정리

--- Codewars_8kyu/quiz/CountingSheep/CountSheep.js
+++ Codewars_8kyu/quiz/CountingSheep/CountSheep.js
@@ -1,17 +1,24 @@
+// const countSheep = list => {
+//     let counter = 0;
+//
+//     if (list == null) {
+//         return counter;
+//     }
+//
+//     for (const aBoolean of list) {
+//         if (aBoolean) {
+//             counter++;
+//         }
+//     }
+//
+//     return counter;
+// };
+
 const countSheep = list => {
-    let counter = 0;
-
     if (list == null) {
-        return counter;
+        return 0;
     }
-
-    for (const aBoolean of list) {
-        if (aBoolean) {
-            counter++;
-        }
-    }
-
-    return counter;
+    return list.filter(Boolean).length;
 };
 
 module.exports = countSheep;
