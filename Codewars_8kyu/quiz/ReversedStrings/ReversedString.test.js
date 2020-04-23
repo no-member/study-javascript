bab51f9499beaf04efc6680d86c9d95efb47a01d
shin <cu7898@gmail.com>
2020-04-24 00:47:39 +0900

    Test #53 테스트 케이스의 추가

--- Codewars_8kyu/quiz/ReversedStrings/ReversedString.test.js
+++ Codewars_8kyu/quiz/ReversedStrings/ReversedString.test.js
@@ -7,3 +7,11 @@ test('reverseString("hi")은 "ih" 반환함',() =>{
 test('reverseString("abcd")은 "dcba" 반환함',() =>{
     expect(reverseString('abcd')).toBe('dcba');
 });
+
+test('reverseString("world")은 "dlrow" 반환함',() =>{
+    expect(reverseString('world')).toBe('dlrow');
+});
+
+test('reverseString("hello")은 "olleh" 반환함',() =>{
+    expect(reverseString('hello')).toBe('olleh');
+});
