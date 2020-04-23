f169c39ead1f53bd6d4961f20657da0bcf39425b
shin <cu7898@gmail.com>
2020-04-24 00:36:42 +0900

    Test #53 reverseString("abcd")은 "dcba" 반환함

--- Codewars_8kyu/quiz/ReversedStrings/ReversedString.test.js
+++ Codewars_8kyu/quiz/ReversedStrings/ReversedString.test.js
@@ -3,3 +3,7 @@ const reverseString = require('./ReversedString');
 test('reverseString("hi")은 "ih" 반환함',() =>{
     expect(reverseString('hi')).toBe('ih');
 });
+
+test('reverseString("abcd")은 "dcba" 반환함',() =>{
+    expect(reverseString('abcd')).toBe('dcba');
+});
