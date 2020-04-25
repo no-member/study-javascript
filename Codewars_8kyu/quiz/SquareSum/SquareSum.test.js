ddd7540af024506c037a9936862ba60d7df98117
shin <cu7898@gmail.com>
2020-04-25 12:25:07 +0900

    Test #59 squareSum([1, 3])은 10을 반환함

--- Codewars_8kyu/quiz/SquareSum/SquareSum.test.js
+++ Codewars_8kyu/quiz/SquareSum/SquareSum.test.js
@@ -3,3 +3,7 @@ const squareSum = require('./SquareSum');
 test('squareSum([1])은 1을 반환함',() =>{
     expect(squareSum([1])).toBe(1);
 });
+
+test('squareSum([1, 3])은 10을 반환함',() =>{
+    expect(squareSum([1, 3])).toBe(10);
+});
