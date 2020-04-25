f0fbad399dce252abb7c0476ee74361ce61500a8
shin <cu7898@gmail.com>
2020-04-25 12:32:53 +0900

    Test #59 테스트 케이스 추가

--- Codewars_8kyu/quiz/SquareSum/SquareSum.test.js
+++ Codewars_8kyu/quiz/SquareSum/SquareSum.test.js
@@ -7,3 +7,11 @@ test('squareSum([1])은 1을 반환함',() =>{
 test('squareSum([1, 3])은 10을 반환함',() =>{
     expect(squareSum([1, 3])).toBe(10);
 });
+
+test('squareSum([1, 3, -9])은 91을 반환함',() =>{
+    expect(squareSum([1, 3, -9])).toBe(91);
+});
+
+test('squareSum([5, 3, -9])은 91을 반환함',() =>{
+    expect(squareSum([5, 3, -9])).toBe(115);
+});
