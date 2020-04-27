19735645d34de1b6bb3a512b8f8a82631ab2239c
shin <cu7898@gmail.com>
2020-04-27 20:42:24 +0900

    Test #61 basicOperation("+", 1, 2)은 3를 반환함

--- Codewars_8kyu/quiz/BasicMathematicalOperations/BasicOperations.test.js
+++ Codewars_8kyu/quiz/BasicMathematicalOperations/BasicOperations.test.js
@@ -1,5 +1,5 @@
 const basicOperation = require('./BasicOperations');
 
-test('basicOperation()은 2를 반환함',() =>{
-    expect(example(1,1)).toBe(2);
+test('basicOperation("+", 1, 2)은 3를 반환함',() =>{
+    expect(basicOperation('+',1, 2)).toBe(3);
 });
