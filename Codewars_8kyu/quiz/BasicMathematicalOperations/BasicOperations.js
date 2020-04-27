c4cd0688ef2a7363c984e6b7b1912d5c605dc9fc
shin <cu7898@gmail.com>
2020-04-27 20:49:57 +0900

    Feat #61 이전 테스트 통과

--- Codewars_8kyu/quiz/BasicMathematicalOperations/BasicOperations.js
+++ Codewars_8kyu/quiz/BasicMathematicalOperations/BasicOperations.js
@@ -2,6 +2,8 @@ const basicOperation = function (operation, num1, num2) {
     switch (operation) {
         case '+':
             return num1 + num2;
+        case '-':
+            return num1 - num2;
     }
     return 0;
 };
