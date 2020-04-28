fea5261bd9333e65b60ee360defd20b32a64a699
shin <cu7898@gmail.com>
2020-04-28 21:15:39 +0900

    Test #63 테스트 케이스의 추가

--- Codewars_8kyu/quiz/ConvertStringNumber/StringToNumber.test.js
+++ Codewars_8kyu/quiz/ConvertStringNumber/StringToNumber.test.js
@@ -7,3 +7,11 @@ test('stringToNumber("1")은 1을 반환함',() =>{
 test('stringToNumber("30")은 30을 반환함',() =>{
     expect(stringToNumber('30')).toBe(30);
 });
+
+test('stringToNumber("1213")은 1213 반환함',() =>{
+    expect(stringToNumber('1213')).toBe(1213);
+});
+
+test('stringToNumber("123123")은 123123을 반환함',() =>{
+    expect(stringToNumber('123123')).toBe(123123);
+});
