c1b3a89f9890d2862ecfb9632d4decb7afc7e776
shin <cu7898@gmail.com>
2020-04-28 21:18:22 +0900

    Test #63 0일 경우 테스트 추가

--- Codewars_8kyu/quiz/ConvertStringNumber/StringToNumber.test.js
+++ Codewars_8kyu/quiz/ConvertStringNumber/StringToNumber.test.js
@@ -1,17 +1,26 @@
 const stringToNumber = require('./StringToNumber');
 
-test('stringToNumber("1")은 1을 반환함',() =>{
-    expect(stringToNumber('1')).toBe(1);
+describe('양수 테스트', ()=>{
+    test('stringToNumber("1")은 1을 반환함',() =>{
+        expect(stringToNumber('1')).toBe(1);
+    });
+
+    test('stringToNumber("30")은 30을 반환함',() =>{
+        expect(stringToNumber('30')).toBe(30);
+    });
+
+    test('stringToNumber("1213")은 1213 반환함',() =>{
+        expect(stringToNumber('1213')).toBe(1213);
+    });
+
+    test('stringToNumber("123123")은 123123을 반환함',() =>{
+        expect(stringToNumber('123123')).toBe(123123);
+    });
 });
 
-test('stringToNumber("30")은 30을 반환함',() =>{
-    expect(stringToNumber('30')).toBe(30);
+describe('0일 경우 테스트', ()=> {
+    test('stringToNumber("0")은 0을 반환함',() =>{
+        expect(stringToNumber('0')).toBe(0);
+    });
 });
 
-test('stringToNumber("1213")은 1213 반환함',() =>{
-    expect(stringToNumber('1213')).toBe(1213);
-});
-
-test('stringToNumber("123123")은 123123을 반환함',() =>{
-    expect(stringToNumber('123123')).toBe(123123);
-});
