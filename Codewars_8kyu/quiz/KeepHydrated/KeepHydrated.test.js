fcf5d7592cccbb53f0045119c5446870fa3d8a1a
shin <cu7898@gmail.com>
2020-04-29 01:04:43 +0900

    Test #67 keepHydrated(1.4)은 0를 반환함

--- Codewars_8kyu/quiz/KeepHydrated/KeepHydrated.test.js
+++ Codewars_8kyu/quiz/KeepHydrated/KeepHydrated.test.js
@@ -3,3 +3,7 @@ const litres = require('./KeepHydrated');
 test('keepHydrated(2)은 1를 반환함',() =>{
     expect(litres(2)).toBe(1);
 });
+
+test('keepHydrated(1.4)은 0를 반환함',() =>{
+    expect(litres(1.4)).toBe(0);
+});
