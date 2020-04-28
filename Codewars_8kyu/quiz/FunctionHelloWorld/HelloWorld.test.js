feb93a19a889f3b4523d0e11da5b9080e6d23844
shin <cu7898@gmail.com>
2020-04-29 01:51:03 +0900

    Test #69 typeof greet는 "function"를 반환한다.

--- Codewars_8kyu/quiz/FunctionHelloWorld/HelloWorld.test.js
+++ Codewars_8kyu/quiz/FunctionHelloWorld/HelloWorld.test.js
@@ -3,3 +3,7 @@ const greet = require('./HelloWorld');
 test('greet()는 "hello world!"를 반환한다.',() =>{
     expect(greet()).toBe("hello world!");
 });
+
+test('typeof greet는 "function"를 반환한다.',() =>{
+    expect(typeof greet).toBe("function");
+});
