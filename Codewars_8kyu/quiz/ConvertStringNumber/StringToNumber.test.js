694834de517369d0c103e0141401a09339389b7f
shin <cu7898@gmail.com>
2020-04-28 21:20:09 +0900

    Test #63 음수일 경우 테스트 추가

--- Codewars_8kyu/quiz/ConvertStringNumber/StringToNumber.test.js
+++ Codewars_8kyu/quiz/ConvertStringNumber/StringToNumber.test.js
@@ -24,3 +24,20 @@ describe('0일 경우 테스트', ()=> {
     });
 });
 
+describe('음수일 경우 테스트', ()=> {
+    test('stringToNumber("-1")은 -1을 반환함',() =>{
+        expect(stringToNumber('-1')).toBe(-1);
+    });
+
+    test('stringToNumber("-30")은 -30을 반환함',() =>{
+        expect(stringToNumber('-30')).toBe(-30);
+    });
+
+    test('stringToNumber("-1213")은 -1213 반환함',() =>{
+        expect(stringToNumber('-1213')).toBe(-1213);
+    });
+
+    test('stringToNumber("-123123")은 -123123을 반환함',() =>{
+        expect(stringToNumber('-123123')).toBe(-123123);
+    });
+});
