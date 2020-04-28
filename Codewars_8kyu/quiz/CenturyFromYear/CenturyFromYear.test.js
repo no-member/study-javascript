2c6906be1b4a94bc723699bb78bb4e847135c801
shin <cu7898@gmail.com>
2020-04-28 22:11:20 +0900

    Test #65 테스트 케이스 추가

--- Codewars_8kyu/quiz/CenturyFromYear/CenturyFromYear.test.js
+++ Codewars_8kyu/quiz/CenturyFromYear/CenturyFromYear.test.js
@@ -7,3 +7,19 @@ test('centuryFromYear(1)은 1를 반환함',() =>{
 test('centuryFromYear(100)은 1를 반환함',() =>{
     expect(centuryFromYear(100)).toBe(1);
 });
+
+test('centuryFromYear(101)은 2를 반환함',() =>{
+    expect(centuryFromYear(100)).toBe(1);
+});
+
+test('centuryFromYear(199)은 1를 반환함',() =>{
+    expect(centuryFromYear(100)).toBe(1);
+});
+
+test('centuryFromYear(200)은 2를 반환함',() =>{
+    expect(centuryFromYear(100)).toBe(1);
+});
+
+test('centuryFromYear(201)은 3를 반환함',() =>{
+    expect(centuryFromYear(100)).toBe(1);
+});
