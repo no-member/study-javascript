a395a17ecb8b0d1954f452ff696880163aa25118
shin <cu7898@gmail.com>
2020-04-28 22:15:13 +0900

    Final #65 최종 정리

--- Codewars_8kyu/quiz/CenturyFromYear/CenturyFromYear.js
+++ Codewars_8kyu/quiz/CenturyFromYear/CenturyFromYear.js
@@ -1,5 +1,6 @@
 const centuryFromYear = function (years) {
-    return Math.floor((years + 99) / 100);
+    // return Math.floor((years + 99) / 100);
+    return Math.ceil(years / 100);
 };
 
 module.exports = centuryFromYear;
