2e88f6bcf1920bc0be239ee1dee91ade9dc3d152
shin <cu7898@gmail.com>
2020-04-29 22:21:50 +0900

    Final #73 최종 정리

--- Codewars_8kyu/quiz/JennySecretMessage/Greeter.js
+++ Codewars_8kyu/quiz/JennySecretMessage/Greeter.js
@@ -1,8 +1,10 @@
 const greet = function (name) {
-    if (name === 'Johnny'){
-        return 'Hello, my love!';
-    }
-    return `Hello, ${name}!`;
+    // if (name === 'Johnny'){
+    //     return 'Hello, my love!';
+    // }
+    // return `Hello, ${name}!`;
+
+   return  name === 'Johnny' ?  'Hello, my love!' : `Hello, ${name}!`;
 };
 
 module.exports = greet;
