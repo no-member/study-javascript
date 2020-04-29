9c9cd41e26baa40bd902118529ff6cc3d56bea54
shin <cu7898@gmail.com>
2020-04-29 23:03:08 +0900

    Test #75 isDivisible(10, 2, 4)은 false를 반환함

--- Codewars_8kyu/quiz/N021N030/N023IsNDivisibleByXAndY/DivisionNumber.test.js
+++ Codewars_8kyu/quiz/N021N030/N023IsNDivisibleByXAndY/DivisionNumber.test.js
@@ -3,3 +3,7 @@ const isDivisible = require('./DivisionNumber');
 test('isDivisible(10, 2, 5)은 true를 반환함',() =>{
     expect(isDivisible(10, 2, 5)).toBe(true);
 });
+
+test('isDivisible(10, 2, 4)은 false를 반환함',() =>{
+    expect(isDivisible(10, 2, 4)).toBe(false);
+});
