d5871cc965927cdee873be227678a55ccd0eebec
shin <cu7898@gmail.com>
2020-04-29 22:57:54 +0900

    Refacotr #75 폴더 정리

---
+++ Codewars_8kyu/quiz/N021N030/N023IsNDivisibleByXAndY/DivisionNumber.js
@@ -0,0 +1,5 @@
+const divisionNumber = function (num1, num2) {
+    return num1 + num2;
+};
+
+module.exports = divisionNumber;
---
+++ Codewars_8kyu/quiz/N021N030/N023IsNDivisibleByXAndY/DivisionNumber.test.js
@@ -0,0 +1,5 @@
+const example = require('./DivisionNumber');
+
+test('example(1,1)은 2를 반환함',() =>{
+    expect(example(1,1)).toBe(2);
+});
