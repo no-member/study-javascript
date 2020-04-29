4933b0aa2e3e7ffe1a2687a81c4abb22b46e780f
shin <cu7898@gmail.com>
2020-04-29 16:12:56 +0900

    Test #71 findNeedleIn 숫자 테스트

--- Codewars_8kyu/quiz/NeedleHaystack/Finder.test.js
+++ Codewars_8kyu/quiz/NeedleHaystack/Finder.test.js
@@ -7,3 +7,7 @@ test('findNeedleIn undefined 테스트',() =>{
 test('findNeedleIn boolean 테스트',() =>{
     expect(findNeedleIn(['3', '123124234', true, 'needle', 'world', 'hay', 2, '3', true, false])).toBe(3);
 });
+
+test('findNeedleIn number 테스트',() =>{
+    expect(findNeedleIn([3, '123124234', true, 'needle', 'world', 'hay', 2, '3', true, false])).toBe(3);
+});
