f2e633ecfeb8ab58b7336163fd251af7add0392c
shin <cu7898@gmail.com>
2020-04-29 21:07:57 +0900

    Test #73 친구들에게 인사를 잘하는지 테스트 케이스 작성

--- Codewars_8kyu/quiz/JennySecretMessage/Greeter.test.js
+++ Codewars_8kyu/quiz/JennySecretMessage/Greeter.test.js
@@ -3,3 +3,11 @@ const greet = require('./Greeter');
 test('greet("Bob")은 "Hello, Bob!"을 반환함',() =>{
     expect(greet('Bob')).toBe('Hello, Bob!');
 });
+
+test('greet("Shin")은 "Hello, Shin!"을 반환함',() =>{
+    expect(greet('Shin')).toBe('Hello, Shin!');
+});
+
+test('greet("Hara")은 "Hello, Hara!"을 반환함',() =>{
+    expect(greet('Hara')).toBe('Hello, Hara!');
+});
