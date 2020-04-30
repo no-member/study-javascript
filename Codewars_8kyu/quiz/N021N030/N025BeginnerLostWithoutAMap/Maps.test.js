bbfe9b2e3f365e2277776fb545cac70242d813d8
shin <cu7898@gmail.com>
2020-05-01 06:31:04 +0900

    Test #79 테스트 케이스 추가

--- Codewars_8kyu/quiz/N021N030/N025BeginnerLostWithoutAMap/Maps.test.js
+++ Codewars_8kyu/quiz/N021N030/N025BeginnerLostWithoutAMap/Maps.test.js
@@ -3,3 +3,15 @@ const doubleList = require('./Maps');
 test('doubleList([1, 2])은 [2, 4]를 반환함',() =>{
     expect(doubleList([1, 2])).toStrictEqual([2, 4]);
 });
+
+test('doubleList([1, 9, 9, 12])은 [2, 18, 18, 24]를 반환함',() =>{
+    expect(doubleList([1, 9, 9, 12])).toStrictEqual([2, 18, 18, 24]);
+});
+
+test('doubleList([1, 10, 0 , 3])은 [2, 20, 0, 6]를 반환함',() =>{
+    expect(doubleList([1, 10, 0, 3])).toStrictEqual([2, 20, 0, 6]);
+});
+
+test('doubleList([1, 5, -2, 3])은 [2, 10, -4, 6]를 반환함',() =>{
+    expect(doubleList([1, 5, -2, 3])).toStrictEqual([2, 10, -4, 6]);
+});
