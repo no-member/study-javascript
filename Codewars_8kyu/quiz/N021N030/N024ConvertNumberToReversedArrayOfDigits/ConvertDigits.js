91f60bd3b49a1ab6d3d2becdf5278a861e7c5076
shin <cu7898@gmail.com>
2020-04-30 20:35:56 +0900

    Final #77 최종 정리

--- Codewars_8kyu/quiz/N021N030/N024ConvertNumberToReversedArrayOfDigits/ConvertDigits.js
+++ Codewars_8kyu/quiz/N021N030/N024ConvertNumberToReversedArrayOfDigits/ConvertDigits.js
@@ -1,10 +1,12 @@
 const toList = function (number) {
-    const answer = [];
-    while (number > 0){
-        answer.push(number % 10);
-        number = Math.floor(number / 10);
-    }
-    return answer;
+    // const answer = [];
+    // while (number > 0){
+    //     answer.push(number % 10);
+    //     number = Math.floor(number / 10);
+    // }
+    // return answer;
+
+    return String(number).split('').map(Number).reverse();
 };
 
 module.exports = toList;
