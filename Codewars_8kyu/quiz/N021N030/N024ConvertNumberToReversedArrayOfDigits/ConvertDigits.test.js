08cbfd1f68679d27a38b018f1366ded224ba6dd0
shin <cu7898@gmail.com>
2020-04-30 20:25:39 +0900

    Test #77 toList(123)은 [3, 2, 1]를 반환함

--- Codewars_8kyu/quiz/N021N030/N024ConvertNumberToReversedArrayOfDigits/ConvertDigits.test.js
+++ Codewars_8kyu/quiz/N021N030/N024ConvertNumberToReversedArrayOfDigits/ConvertDigits.test.js
@@ -3,3 +3,7 @@ const toList = require('./ConvertDigits');
 test('toList(12)은 [2, 1]를 반환함',() =>{
     expect(toList(12)).toStrictEqual([2, 1]);
 });
+
+test('toList(123)은 [3, 2, 1]를 반환함',() =>{
+    expect(toList(123)).toStrictEqual([3, 2, 1]);
+});
