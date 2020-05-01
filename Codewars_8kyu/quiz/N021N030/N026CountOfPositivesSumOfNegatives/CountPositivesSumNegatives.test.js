70d87c83a7c352b5d49a90857b48c4820b020c21
shin <cu7898@gmail.com>
2020-05-01 09:29:57 +0900

    Test #81 countPositivesSumNegatives([2, 1, 3, -3 -1])은 [6, -4]를 반환함

--- Codewars_8kyu/quiz/N021N030/N026CountOfPositivesSumOfNegatives/CountPositivesSumNegatives.test.js
+++ Codewars_8kyu/quiz/N021N030/N026CountOfPositivesSumOfNegatives/CountPositivesSumNegatives.test.js
@@ -1,5 +1,9 @@
 const countPositivesSumNegatives = require('./CountPositivesSumNegatives');
 
 test('countPositivesSumNegatives([1, 2, -1])은 [3, -1]를 반환함',() =>{
-    expect(countPositivesSumNegatives([1, 2, -1])).toBe([3, -1]);
+    expect(countPositivesSumNegatives([1, 2, -1])).toStrictEqual([3, -1]);
+});
+
+test('countPositivesSumNegatives([2, 1, 3, -3 -1])은 [6, -4]를 반환함',() =>{
+    expect(countPositivesSumNegatives([2, 1, 3, -3, -1])).toStrictEqual([6, -4]);
 });
