ca5ae809b333d73f26b8d5a4bfe2d752696dfdbc
shin <cu7898@gmail.com>
2020-05-01 09:40:03 +0900

    Test #81 0을 잘 처리하는지 테스트

--- Codewars_8kyu/quiz/N021N030/N026CountOfPositivesSumOfNegatives/CountPositivesSumNegatives.test.js
+++ Codewars_8kyu/quiz/N021N030/N026CountOfPositivesSumOfNegatives/CountPositivesSumNegatives.test.js
@@ -7,3 +7,7 @@ test('countPositivesSumNegatives([1, 2, -1])은 [2, -1]를 반환함',() =>{
 test('countPositivesSumNegatives([2, 1, 3, -3 -1])은 [3, -4]를 반환함',() =>{
     expect(countPositivesSumNegatives([2, 1, 3, -3, -1])).toStrictEqual([3, -4]);
 });
+
+test('countPositivesSumNegatives([2, 1, 3, 0, -3 -1])은 [3, -4]를 반환함',() =>{
+    expect(countPositivesSumNegatives([2, 1, 3, 0, -3, -1])).toStrictEqual([3, -4]);
+});
