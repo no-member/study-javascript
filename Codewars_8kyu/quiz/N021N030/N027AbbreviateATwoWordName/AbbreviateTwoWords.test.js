69a19127cf79653ea6bc3d2b4af08143aae2de3d
shin <cu7898@gmail.com>
2020-05-02 11:13:48 +0900

    Test #83 테스트 케이스 추가

--- Codewars_8kyu/quiz/N021N030/N027AbbreviateATwoWordName/AbbreviateTwoWords.test.js
+++ Codewars_8kyu/quiz/N021N030/N027AbbreviateATwoWordName/AbbreviateTwoWords.test.js
@@ -7,3 +7,15 @@ test('abbreviateTwoWords("Sam Harris")은 S.H를 반환함',() =>{
 test('abbreviateTwoWords("Patrick Feenan")은 P.F를 반환함',() =>{
     expect(abbreviateTwoWords('Patrick Feenan')).toBe('P.F');
 });
+
+test('abbreviateTwoWords("Evan Cole")은 E.C를 반환함',() =>{
+    expect(abbreviateTwoWords('Evan Cole')).toBe('E.C');
+});
+
+test('abbreviateTwoWords("P Favuzzi")은 P.F를 반환함',() =>{
+    expect(abbreviateTwoWords('P Favuzzi')).toBe('P.F');
+});
+
+test('abbreviateTwoWords("David Mendieta")은 D.M를 반환함',() =>{
+    expect(abbreviateTwoWords('David Mendieta')).toBe('D.M');
+});
