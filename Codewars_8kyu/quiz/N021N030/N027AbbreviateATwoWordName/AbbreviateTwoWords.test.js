c3360e1fba6b56dfaec8123453f082da114b3ccd
shin <cu7898@gmail.com>
2020-05-02 11:14:39 +0900

    Test #83 abbreviateTwoWords("sam harris")은 S.H를 반환함

--- Codewars_8kyu/quiz/N021N030/N027AbbreviateATwoWordName/AbbreviateTwoWords.test.js
+++ Codewars_8kyu/quiz/N021N030/N027AbbreviateATwoWordName/AbbreviateTwoWords.test.js
@@ -4,6 +4,10 @@ test('abbreviateTwoWords("Sam Harris")은 S.H를 반환함',() =>{
     expect(abbreviateTwoWords('Sam Harris')).toBe('S.H');
 });
 
+test('abbreviateTwoWords("sam harris")은 S.H를 반환함',() =>{
+    expect(abbreviateTwoWords('sam harris')).toBe('S.H');
+});
+
 test('abbreviateTwoWords("Patrick Feenan")은 P.F를 반환함',() =>{
     expect(abbreviateTwoWords('Patrick Feenan')).toBe('P.F');
 });
