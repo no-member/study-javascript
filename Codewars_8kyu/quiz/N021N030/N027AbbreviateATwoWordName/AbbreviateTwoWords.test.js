4aa4e86e9fca24b0fb53424bdaf73e6f146b3b9e
shin <cu7898@gmail.com>
2020-05-02 11:07:39 +0900

    Test #83 abbreviateTwoWords("Patrick Feenan")은 P.F를 반환

--- Codewars_8kyu/quiz/N021N030/N027AbbreviateATwoWordName/AbbreviateTwoWords.test.js
+++ Codewars_8kyu/quiz/N021N030/N027AbbreviateATwoWordName/AbbreviateTwoWords.test.js
@@ -3,3 +3,7 @@ const abbreviateTwoWords = require('./AbbreviateTwoWords');
 test('abbreviateTwoWords("Sam Harris")은 S.H를 반환함',() =>{
     expect(abbreviateTwoWords('Sam Harris')).toBe('S.H');
 });
+
+test('abbreviateTwoWords("Patrick Feenan")은 P.F를 반환함',() =>{
+    expect(abbreviateTwoWords('Patrick Feenan')).toBe('P.F');
+});
