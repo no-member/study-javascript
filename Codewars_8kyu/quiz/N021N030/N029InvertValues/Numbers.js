ea4911337c00b00c9f223522a1be1d2923bf3ad2
shin <cu7898@gmail.com>
2020-05-04 08:54:16 +0900

    Feat #87 0일 때 대응 추가

--- Codewars_8kyu/quiz/N021N030/N029InvertValues/Numbers.js
+++ Codewars_8kyu/quiz/N021N030/N029InvertValues/Numbers.js
@@ -1,5 +1,10 @@
 const invert = function (numbers) {
-    return numbers.map(n => -n);
+    return numbers.map(function (n) {
+        if (n !== 0) {
+            return -n;
+        }
+        return n;
+    });
 };
 
 module.exports = invert;
