63835d2cbfd47ebb1a7333e328b06cac0e64a642
shin <cu7898@gmail.com>
2020-05-03 11:23:29 +0900

    Feat #85 해결 방법 추가

--- Codewars_8kyu/quiz/N021N030/N028CountTheMonkeys/MonkeyCounter.js
+++ Codewars_8kyu/quiz/N021N030/N028CountTheMonkeys/MonkeyCounter.js
@@ -1,11 +1,13 @@
 const monkeyCounter = function (number) {
-    const answer = [];
-
-    for (let i = 0; i < number; i++) {
-       answer[i] = i + 1;
-    }
-
-    return answer;
+    // const answer = [];
+    //
+    // for (let i = 0; i < number; i++) {
+    //    answer.push(i + 1);
+    // }
+    //
+    // return answer;
+    
+    return [...Array(number).keys()].map(n => n +1);
 };
 
 module.exports = monkeyCounter;
