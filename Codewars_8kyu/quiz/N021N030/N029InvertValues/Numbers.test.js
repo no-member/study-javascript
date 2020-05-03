4620674cb9986db48a99680664dd1116a97f2dc1
shin <cu7898@gmail.com>
2020-05-03 15:36:06 +0900

    Test #87 invert([1, 2, 10])은 [-1, -2, -10]를 반환함

--- Codewars_8kyu/quiz/N021N030/N029InvertValues/Numbers.test.js
+++ Codewars_8kyu/quiz/N021N030/N029InvertValues/Numbers.test.js
@@ -3,3 +3,7 @@ const invert = require('./Numbers');
 test('invert([1, 2])은 [-1, -2]를 반환함',() =>{
     expect(invert([1, 2])).toStrictEqual([-1, -2]);
 });
+
+test('invert([1, 2, 10])은 [-1, -2, -10]를 반환함',() =>{
+    expect(invert([1, 2, 10])).toStrictEqual([-1, -2, -10]);
+});
