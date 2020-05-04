1734eb9b45ab9761f4eb6d3c214dfb4e5bee12cc
shin <cu7898@gmail.com>
2020-05-04 09:03:14 +0900

    Final #87 최종정리

--- Codewars_8kyu/quiz/N021N030/N029InvertValues/Numbers.js
+++ Codewars_8kyu/quiz/N021N030/N029InvertValues/Numbers.js
@@ -1,13 +1,14 @@
-const invertExcept0 = (number) => {
-    if (number !== 0) {
-        return -number;
-    }
-
-    return number;
-};
+// const invertExcept0 = (number) => {
+//     if (number !== 0) {
+//         return -number;
+//     }
+//
+//     return number;
+// };
 
 const invert = (numbers) => {
-    return numbers.map(invertExcept0);
+    // return numbers.map(invertExcept0);
+    return numbers.map(n => n === 0 ? n : -n);
 };
 
 module.exports = invert;
