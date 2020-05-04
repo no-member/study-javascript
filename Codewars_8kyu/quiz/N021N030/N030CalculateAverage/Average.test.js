0b00761917eefa98c958b4effd57ecaccc35b2ab
shin <cu7898@gmail.com>
2020-05-04 10:30:18 +0900

    Final #88 최종 정리

--- Codewars_8kyu/quiz/N021N030/N030CalculateAverage/Average.test.js
+++ Codewars_8kyu/quiz/N021N030/N030CalculateAverage/Average.test.js
@@ -1,18 +1,18 @@
 const calculate = require('./Average');
 
-test('calculate([1,1])은 1를 반환함',() =>{
-    expect(calculate([1,1])).toBe(1);
+test('calculate([1,1])은 1를 반환함', () => {
+    expect(calculate([1, 1])).toBe(1);
 });
 
 
-test('calculate([1, 2, 2, 1])은 1.5를 반환함',() =>{
+test('calculate([1, 2, 2, 1])은 1.5를 반환함', () => {
     expect(calculate([1, 2, 2, 1])).toBe(1.5);
 });
 
-test('calculate([0])은 0를 반환함',() =>{
+test('calculate([0])은 0를 반환함', () => {
     expect(calculate([0])).toBe(0);
 });
 
-test('calculate([])은 0를 반환함',() =>{
+test('calculate([])은 0를 반환함', () => {
     expect(calculate([])).toBe(0);
 });
