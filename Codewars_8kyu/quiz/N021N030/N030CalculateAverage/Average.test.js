b36f911eda266417cfb19b5d7ab6e4c27ab96d3f
shin <cu7898@gmail.com>
2020-05-04 10:23:44 +0900

    Test #88 0을 대입했을 때 테스트

--- Codewars_8kyu/quiz/N021N030/N030CalculateAverage/Average.test.js
+++ Codewars_8kyu/quiz/N021N030/N030CalculateAverage/Average.test.js
@@ -3,3 +3,7 @@ const calculate = require('./Average');
 test('calculate([1,1])은 1를 반환함',() =>{
     expect(calculate([1,1])).toBe(1);
 });
+
+test('calculate([0])은 0를 반환함',() =>{
+    expect(calculate([0])).toBe(0);
+});
