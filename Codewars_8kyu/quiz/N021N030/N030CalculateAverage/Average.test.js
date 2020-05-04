9de056f619f3931f6ad3bed69cd9387141d51d86
shin <cu7898@gmail.com>
2020-05-04 10:28:40 +0900

    Test #88 소수점 자리가 있는 평균을 구하는 테스트

--- Codewars_8kyu/quiz/N021N030/N030CalculateAverage/Average.test.js
+++ Codewars_8kyu/quiz/N021N030/N030CalculateAverage/Average.test.js
@@ -4,6 +4,11 @@ test('calculate([1,1])은 1를 반환함',() =>{
     expect(calculate([1,1])).toBe(1);
 });
 
+
+test('calculate([1, 2, 2, 1])은 1.5를 반환함',() =>{
+    expect(calculate([1, 2, 2, 1])).toBe(1.5);
+});
+
 test('calculate([0])은 0를 반환함',() =>{
     expect(calculate([0])).toBe(0);
 });
