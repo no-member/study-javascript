dd6690bcff3291b7828f6be4fac086f63cbff905
shin <cu7898@gmail.com>
2020-05-05 13:09:55 +0900

    Test #93 테스트 케이스 추가

--- Codewars_8kyu/quiz/N031N040/N032DoIGetABonus/Bonus.test.js
+++ Codewars_8kyu/quiz/N031N040/N032DoIGetABonus/Bonus.test.js
@@ -3,3 +3,11 @@ const bonusCalculate = require('./Bonus');
 test('bonusCalculate(10, false)은 £10를 반환함',() =>{
     expect(bonusCalculate(10, false)).toBe('£10');
 });
+
+test('bonusCalculate(200, false)은 £200를 반환함',() =>{
+    expect(bonusCalculate(200, false)).toBe('£200');
+});
+
+test('bonusCalculate(333, false)은 £333 반환함',() =>{
+    expect(bonusCalculate(333, false)).toBe('£333');
+});
