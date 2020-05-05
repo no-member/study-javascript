2f10eaa9041ce4cf85ab85afca005bcbc75e41be
shin <cu7898@gmail.com>
2020-05-05 12:25:27 +0900

    Test #91 테스트 케이스 추가

--- Codewars_8kyu/quiz/N031N040/N031YouCantCodeUnderPressure/UnderPressure.test.js
+++ Codewars_8kyu/quiz/N031N040/N031YouCantCodeUnderPressure/UnderPressure.test.js
@@ -3,3 +3,11 @@ const doubleInteger = require('./UnderPressure');
 test('doubleInteger(2)은 4를 반환함',() =>{
     expect(doubleInteger(2)).toBe(4);
 });
+
+test('doubleInteger(10)은 20를 반환함',() =>{
+    expect(doubleInteger(10)).toBe(20);
+});
+
+test('doubleInteger(104)은 208를 반환함',() =>{
+    expect(doubleInteger(104)).toBe(208);
+});
