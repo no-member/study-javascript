4c876f69978f7bcf67e10544b92f33d66c93ab11
shin <cu7898@gmail.com>
2020-05-06 17:55:26 +0900

    Test #95 dnaToRna("TTTT")은 "UUUU"를 반환함

--- Codewars_8kyu/quiz/N031N040/N033DNAToRNAConversion/Bio.test.js
+++ Codewars_8kyu/quiz/N031N040/N033DNAToRNAConversion/Bio.test.js
@@ -7,3 +7,7 @@ test('dnaToRna("T")은 "U"를 반환함',() =>{
 test('dnaToRna("TT")은 "UU"를 반환함',() =>{
     expect(dnaToRna('TT')).toBe('UU');
 });
+
+test('dnaToRna("TTTT")은 "UUUU"를 반환함',() =>{
+    expect(dnaToRna('TTTT')).toBe('UUUU');
+});
