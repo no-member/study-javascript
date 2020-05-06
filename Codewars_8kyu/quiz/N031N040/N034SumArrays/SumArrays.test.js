019ffe83cc8c07edb2f43590929f5c79352477bf
shin <cu7898@gmail.com>
2020-05-06 18:16:26 +0900

    Test #97 sum([1, 3])은 4를 반환함

--- Codewars_8kyu/quiz/N031N040/N034SumArrays/SumArrays.test.js
+++ Codewars_8kyu/quiz/N031N040/N034SumArrays/SumArrays.test.js
@@ -3,3 +3,7 @@ const sum = require('./SumArrays');
 test('sum([1])은 1를 반환함', () => {
     expect(sum([1])).toBe(1);
 });
+
+test('sum([1, 3])은 4를 반환함', () => {
+    expect(sum([1, 3])).toBe(4);
+});
