5fbf6e46ccd80a1d8cb814b8a3f6bee170eb496b
shin <cu7898@gmail.com>
2020-05-06 19:34:29 +0900

    TestF #99 areYouPlayingBanjo("Mark")은 Mark does not play banjo를 반환함

--- Codewars_8kyu/quiz/N031N040/N035AreYouPlayingBanjo/Banjo.test.js
+++ Codewars_8kyu/quiz/N031N040/N035AreYouPlayingBanjo/Banjo.test.js
@@ -3,3 +3,7 @@ const areYouPlayingBanjo = require('./Banjo');
 test('areYouPlayingBanjo("Martin")은 Martin does not play banjo를 반환함',() =>{
     expect(areYouPlayingBanjo("Martin")).toBe('Martin does not play banjo');
 });
+
+test('areYouPlayingBanjo("Mark")은 Mark does not play banjo를 반환함',() =>{
+    expect(areYouPlayingBanjo("Mark")).toBe('Mark does not play banjo');
+});
