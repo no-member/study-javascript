8e82739788e353bdd6adb1991dde0822d701b172
shin <cu7898@gmail.com>
2020-05-06 17:51:40 +0900

    Test #95 dnaToRna("TT")은 "UU"를 반환함

--- Codewars_8kyu/quiz/N031N040/N033DNAToRNAConversion/Bio.test.js
+++ Codewars_8kyu/quiz/N031N040/N033DNAToRNAConversion/Bio.test.js
@@ -3,3 +3,7 @@ const dnaToRna = require('./Bio');
 test('dnaToRna("T")은 "U"를 반환함',() =>{
     expect(dnaToRna('T')).toBe('U');
 });
+
+test('dnaToRna("TT")은 "UU"를 반환함',() =>{
+    expect(dnaToRna('TT')).toBe('UU');
+});
