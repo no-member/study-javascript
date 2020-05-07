91992a2b322ba49de425882ce4a3b884ea952953
shin <cu7898@gmail.com>
2020-05-07 21:16:56 +0900

    TestP #101 reverse("")은 ""를 반환함

--- Codewars_8kyu/quiz/N031N040/N036ReversedWords/Words.test.js
+++ Codewars_8kyu/quiz/N031N040/N036ReversedWords/Words.test.js
@@ -8,6 +8,10 @@ test('reverse("hi world!")은 "world! hi"를 반환함',() =>{
     expect(reverse('hi world!')).toBe('world! hi');
 });
 
-test('reverse(foobar")은 "foobar"를 반환함',() =>{
+test('reverse("foobar")은 "foobar"를 반환함',() =>{
     expect(reverse('foobar')).toBe('foobar');
+});
+
+test('reverse("")은 ""를 반환함',() =>{
+    expect(reverse('')).toBe('');
 });
