160581ce674fd8763fb1cf4613fce73c2b146e20
shin <cu7898@gmail.com>
2020-05-07 21:22:01 +0900

    Final #101 최종 정리

--- Codewars_8kyu/quiz/N031N040/N036ReversedWords/Words.test.js
+++ Codewars_8kyu/quiz/N031N040/N036ReversedWords/Words.test.js
@@ -1,21 +1,21 @@
 const reverse = require('./Words');
 
-test('reverse("hello world!")은 "world! hello"를 반환함',() =>{
+test('reverse("hello world!")은 "world! hello"를 반환함', () => {
     expect(reverse('hello world!')).toBe('world! hello');
 });
 
-test('reverse("hi world!")은 "world! hi"를 반환함',() =>{
+test('reverse("hi world!")은 "world! hi"를 반환함', () => {
     expect(reverse('hi world!')).toBe('world! hi');
 });
 
-test('reverse("foobar")은 "foobar"를 반환함',() =>{
+test('reverse("foobar")은 "foobar"를 반환함', () => {
     expect(reverse('foobar')).toBe('foobar');
 });
 
-test('reverse("")은 ""를 반환함',() =>{
+test('reverse("")은 ""를 반환함', () => {
     expect(reverse('')).toBe('');
 });
 
-test('reverse()은 ""를 반환함',() =>{
+test('reverse()은 ""를 반환함', () => {
     expect(reverse()).toBe('');
 });
