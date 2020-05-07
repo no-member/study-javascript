c6d7201a4c74e40cfabfdc75e6cf28eb4bf5d3b1
shin <cu7898@gmail.com>
2020-05-07 21:15:09 +0900

    TestP #101 reverse(foobar")은 "foobar"를 반환함

--- Codewars_8kyu/quiz/N031N040/N036ReversedWords/Words.test.js
+++ Codewars_8kyu/quiz/N031N040/N036ReversedWords/Words.test.js
@@ -7,3 +7,7 @@ test('reverse("hello world!")은 "world! hello"를 반환함',() =>{
 test('reverse("hi world!")은 "world! hi"를 반환함',() =>{
     expect(reverse('hi world!')).toBe('world! hi');
 });
+
+test('reverse(foobar")은 "foobar"를 반환함',() =>{
+    expect(reverse('foobar')).toBe('foobar');
+});
