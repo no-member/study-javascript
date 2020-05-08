96534a3a20c016d166b1d3398a87af9791f640b7
shin <cu7898@gmail.com>
2020-05-09 03:33:31 +0900

    TestP #101 sumWithoutMinMax 일반적인 테스트

--- Codewars_8kyu/quiz/N031N040/N037SumWithoutHighestAndLowestNumber/Example.test.js
+++ Codewars_8kyu/quiz/N031N040/N037SumWithoutHighestAndLowestNumber/Example.test.js
@@ -3,3 +3,11 @@ const sumWithoutMinMax = require('./SumWithoutMinMax');
 test('sumWithoutMinMax([1, 2, 3])은 2를 반환함',() =>{
     expect(sumWithoutMinMax([1, 2, 3])).toBe(2);
 });
+
+test('sumWithoutMinMax([1, 2, -10, 3, 999])은 6를 반환함',() =>{
+    expect(sumWithoutMinMax([1, 2, -10, 3, 999])).toBe(6);
+});
+
+test('sumWithoutMinMax([1, 999])은 0를 반환함',() =>{
+    expect(sumWithoutMinMax([1, 999])).toBe(0);
+});
