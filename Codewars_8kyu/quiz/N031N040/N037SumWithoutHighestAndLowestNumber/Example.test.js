e57b3a625f83cb77874e086e20c09d753fa42d85
shin <cu7898@gmail.com>
2020-05-09 03:34:38 +0900

    Feat #103 sumWithoutMinMax 원소가 1개인 리스트일 경우

--- Codewars_8kyu/quiz/N031N040/N037SumWithoutHighestAndLowestNumber/Example.test.js
+++ Codewars_8kyu/quiz/N031N040/N037SumWithoutHighestAndLowestNumber/Example.test.js
@@ -11,3 +11,7 @@ test('sumWithoutMinMax([1, 2, -10, 3, 999])은 6를 반환함',() =>{
 test('sumWithoutMinMax([1, 999])은 0를 반환함',() =>{
     expect(sumWithoutMinMax([1, 999])).toBe(0);
 });
+
+test('sumWithoutMinMax([999])은 0를 반환함',() =>{
+    expect(sumWithoutMinMax([999])).toBe(0);
+});
--- Codewars_8kyu/quiz/N031N040/N037SumWithoutHighestAndLowestNumber/SumWithoutMinMax.js
+++ Codewars_8kyu/quiz/N031N040/N037SumWithoutHighestAndLowestNumber/SumWithoutMinMax.js
@@ -1,4 +1,7 @@
 const sumWithoutMinMax = function (numbers) {
+    if (numbers.length <= 2) {
+        return 0;
+    }
     return numbers.reduce((a, b) => a + b, 0) - Math.min(...numbers) - Math.max(...numbers);
 };
 
