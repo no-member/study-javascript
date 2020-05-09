7ef723a453ab7b473143602c88635d564c1dcfc7
shin <cu7898@gmail.com>
2020-05-09 09:40:10 +0900

    Refactor #105 describe 추가, 중복을 함수로 빼냄

--- Codewars_8kyu/quiz/N031N040/N038FindMaximumAndMinimumValuesOfAList/NumberUtils.js
+++ Codewars_8kyu/quiz/N031N040/N038FindMaximumAndMinimumValuesOfAList/NumberUtils.js
@@ -1,12 +1,16 @@
+const isNullOrEmpty = function (list) {
+    return !list || list.length === 0;
+};
+
 const max = function (numbers) {
-    if (!numbers || numbers.length === 0) {
+    if (isNullOrEmpty(numbers)) {
         return 0;
     }
     return Math.max(...numbers);
 };
 
 const min = function (numbers) {
-    if (!numbers || numbers.length === 0) {
+    if (isNullOrEmpty(numbers)) {
         return 0;
     }
     return Math.min(...numbers);
--- Codewars_8kyu/quiz/N031N040/N038FindMaximumAndMinimumValuesOfAList/NumberUtils.test.js
+++ Codewars_8kyu/quiz/N031N040/N038FindMaximumAndMinimumValuesOfAList/NumberUtils.test.js
@@ -1,42 +1,46 @@
 const {max} = require('./NumberUtils');
 const {min} = require('./NumberUtils');
 
-test('max([1, 2])은 2를 반환함', () => {
-    expect(max([1, 2])).toBe(2);
+describe('max 테스트', () => {
+    test('max([1, 2])은 2를 반환함', () => {
+        expect(max([1, 2])).toBe(2);
+    });
+
+    test('max([100, 2, 0, -10, 999])은 999를 반환함', () => {
+        expect(max([100, 2, 0, -10, 999])).toBe(999);
+    });
+
+    test('max([-2, -10, -999])은 -2를 반환함', () => {
+        expect(max([-2, -10, -999])).toBe(-2);
+    });
+
+    test('max([])은 0를 반환함', () => {
+        expect(max([])).toBe(0);
+    });
+
+    test('max(null)은 0를 반환함', () => {
+        expect(max(null)).toBe(0);
+    });
 });
 
-test('max([100, 2, 0, -10, 999])은 999를 반환함', () => {
-    expect(max([100, 2, 0, -10, 999])).toBe(999);
-});
+describe('min 테스트', () => {
+    test('min([1, 2])은 1를 반환함', () => {
+        expect(min([1, 2])).toBe(1);
+    });
 
-test('max([-2, -10, -999])은 -2를 반환함', () => {
-    expect(max([-2, -10, -999])).toBe(-2);
-});
+    test('min([100, 2, 0, -10, 999])은 -10을 반환함', () => {
+        expect(min([100, 2, 0, -10, 999])).toBe(-10);
+    });
 
-test('max([])은 0를 반환함', () => {
-    expect(max([])).toBe(0);
-});
+    test('min([-2, -10, -999])은 -999를 반환함', () => {
+        expect(min([-2, -10, -999])).toBe(-999);
+    });
 
-test('max(null)은 0를 반환함', () => {
-    expect(max(null)).toBe(0);
-});
+    test('min([])은 0를 반환함', () => {
+        expect(min([])).toBe(0);
+    });
 
-test('min([1, 2])은 1를 반환함', () => {
-    expect(min([1, 2])).toBe(1);
-});
-
-test('min([100, 2, 0, -10, 999])은 -10을 반환함', () => {
-    expect(min([100, 2, 0, -10, 999])).toBe(-10);
-});
-
-test('min([-2, -10, -999])은 -999를 반환함', () => {
-    expect(min([-2, -10, -999])).toBe(-999);
-});
-
-test('min([])은 0를 반환함', () => {
-    expect(min([])).toBe(0);
-});
-
-test('min(null)은 0를 반환함', () => {
-    expect(min(null)).toBe(0);
+    test('min(null)은 0를 반환함', () => {
+        expect(min(null)).toBe(0);
+    });
 });
