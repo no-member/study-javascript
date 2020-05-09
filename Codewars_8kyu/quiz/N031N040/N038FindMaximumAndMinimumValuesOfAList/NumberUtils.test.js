bf0a056c0a47fd2037377fa8293dad46296d24cb
shin <cu7898@gmail.com>
2020-05-09 09:25:23 +0900

    TestP #105 max 테스트 케이스 추가

--- Codewars_8kyu/quiz/N031N040/N038FindMaximumAndMinimumValuesOfAList/NumberUtils.test.js
+++ Codewars_8kyu/quiz/N031N040/N038FindMaximumAndMinimumValuesOfAList/NumberUtils.test.js
@@ -3,3 +3,11 @@ const max = require('./NumberUtils');
 test('max([1, 2])은 2를 반환함', () => {
     expect(max([1, 2])).toBe(2);
 });
+
+test('max([100, 2, 0, -10, 999])은 999를 반환함', () => {
+    expect(max([100, 2, 0, -10, 999])).toBe(999);
+});
+
+test('max([-2, -10, -999])은 -2를 반환함', () => {
+    expect(max([-2, -10, -999])).toBe(-2);
+});
