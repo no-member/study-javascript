0a92520a6e8667501b094768ed6488090ad087d1
shin <cu7898@gmail.com>
2020-05-11 01:49:05 +0900

    TIL 자바스크립트 핵심 가이드 U3C9

---
+++ DouglasCrockford_JavaScript_The_Good_Parts/Unit03/Chapter09/Global.js
@@ -0,0 +1,81 @@
+const stooge = {
+    "first-name": "Jerome",
+    "last-name": "Howard",
+};
+
+const flight = {
+    airline: "Oceanic",
+    number: 815,
+    departure: {
+        IATA: "SYD",
+        time: "2004-09-23 14:55",
+        city: "Sydney"
+    },
+    arrival: {
+        IATA: "LAX",
+        time: "2004-09-23 10:42",
+        city: "Los Angeles"
+    }
+};
+
+stooge['first-name'] = 'James';
+
+stooge['middle-name'] = 'Lester';
+
+stooge.nickname = 'Curly';
+
+flight.equipment = {
+    model: 'Boeing 777'
+};
+
+flight.status = 'overdue';
+
+const x = stooge;
+x.nickname = 'Curly2';
+
+const nick = stooge.nickname;
+
+let a = {}, b = {}, c = {};
+
+a = b = c = {};
+
+if (typeof Object.create !== 'function') {
+    Object.create = function (object) {
+        const F = function () {
+        };
+        F.prototype = object;
+        return new F();
+    }
+}
+
+const another_stooge = Object.create(stooge);
+
+another_stooge["first-name"] = 'Harry';
+another_stooge["middle-name"] = 'Moses';
+another_stooge.nickname = 'Moe';
+
+stooge.profession = 'actor';
+
+delete another_stooge.nickname;
+
+let MYAPP = {};
+
+MYAPP.stooge = {
+    'first-name' : 'Joe',
+    'last-name' : 'Howard'
+};
+
+MYAPP.flight = {
+    airline: 'Oceanic',
+    number: 815,
+    departure: {
+        IATA: "SYD",
+        time: '2004-09-22 14:55',
+        city: 'Sydney'
+    },
+    arrival: {
+        IATA: "LAX",
+        time: "2004-09-23 10:42",
+        city: "Los Angeles"
+    },
+};
