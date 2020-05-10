185a1bbf1deff0a2370a0ef3b423d5e94c3aa352
shin <cu7898@gmail.com>
2020-05-10 20:38:41 +0900

    TIL 자바스크립트 핵심 가이드 U4C5

--- DouglasCrockford_JavaScript_The_Good_Parts/Unit03/Chapter05/Prototype.js
+++ DouglasCrockford_JavaScript_The_Good_Parts/Unit03/Chapter05/Prototype.js
@@ -1,7 +1,6 @@
 const stooge = {
     "first-name": "Jerome",
     "last-name": "Howard",
-    "": "my Boss",
 };
 
 const flight = {
@@ -35,9 +34,36 @@ const x = stooge;
 x.nickname = 'Curly2';
 
 const nick = stooge.nickname;
-console.log(nick);
 
 let a = {}, b = {}, c = {};
 
 a = b = c = {};
 
+if (typeof Object.create !== 'function') {
+    Object.create = function (object) {
+        const F = function () {
+        };
+        F.prototype = object;
+        return new F();
+    }
+}
+
+const another_stooge = Object.create(stooge);
+
+console.log(another_stooge);
+
+another_stooge["first-name"] = 'Harry';
+another_stooge["middle-name"] = 'Moses';
+another_stooge.nickname = 'Moe';
+
+console.log(another_stooge);
+
+// 위에서는 last-name이 없었지만 prototype 내에 있으므로 Howard를 잘 불러옴
+console.log(another_stooge["last-name"]);
+
+console.log(stooge);
+
+stooge.profession = 'actor';
+console.log(stooge);
+
+console.log(another_stooge.profession);
