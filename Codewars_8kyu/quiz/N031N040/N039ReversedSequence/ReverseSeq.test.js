6d1d7a91424025b0e91913eae2188b0a568ea6fb
shin <cu7898@gmail.com>
2020-05-10 16:50:13 +0900

    TestP #107 reverseSeq 테스트

--- Codewars_8kyu/quiz/N031N040/N039ReversedSequence/ReverseSeq.test.js
+++ Codewars_8kyu/quiz/N031N040/N039ReversedSequence/ReverseSeq.test.js
@@ -3,3 +3,7 @@ const reverseSeq = require('./ReverseSeq');
 test('reverseSeq(2)은 [2,1]를 반환함', () => {
     expect(reverseSeq(2)).toStrictEqual([2, 1]);
 });
+
+test('reverseSeq(5)은 [5, 4, 3, 2,1]를 반환함', () => {
+    expect(reverseSeq(5)).toStrictEqual([5, 4, 3, 2, 1]);
+});
