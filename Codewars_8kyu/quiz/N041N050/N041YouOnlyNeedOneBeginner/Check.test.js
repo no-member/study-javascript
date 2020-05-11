39fee335f715a159fa4d1fa229e7021d6cfe4ea4
shin <shin@value-it.co.jp>
2020-05-11 22:19:17 +0900

    TestP #111 check 테스트

--- Codewars_8kyu/quiz/N041N050/N041YouOnlyNeedOneBeginner/Check.test.js
+++ Codewars_8kyu/quiz/N041N050/N041YouOnlyNeedOneBeginner/Check.test.js
@@ -3,3 +3,7 @@ const check = require('./Check');
 test('check([1, 2], 2)은 true를 반환함', () => {
     expect(check([1, 2], 2)).toBe(true);
 });
+
+test('check([1, 2, 10], 0)은 false를 반환함', () => {
+    expect(check([1, 2, 10], 0)).toBe(false);
+});
