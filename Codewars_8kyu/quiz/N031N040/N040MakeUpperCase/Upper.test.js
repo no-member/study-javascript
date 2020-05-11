26006274002353f49cb2714b589a3886b273f547
shin <cu7898@gmail.com>
2020-05-11 09:07:56 +0900

    TestP #109 makeUpperCase 테스트 통과

--- Codewars_8kyu/quiz/N031N040/N040MakeUpperCase/Upper.test.js
+++ Codewars_8kyu/quiz/N031N040/N040MakeUpperCase/Upper.test.js
@@ -3,3 +3,11 @@ const makeUpperCase = require('./Upper');
 test('makeUpperCase("hello")은 "HELLO"를 반환함',() =>{
     expect(makeUpperCase('hello')).toBe('HELLO');
 });
+
+test('makeUpperCase("hi")은 "HI"를 반환함',() =>{
+    expect(makeUpperCase('hi')).toBe('HI');
+});
+
+test('makeUpperCase("my")은 "MY"를 반환함',() =>{
+    expect(makeUpperCase('my')).toBe('MY');
+});
