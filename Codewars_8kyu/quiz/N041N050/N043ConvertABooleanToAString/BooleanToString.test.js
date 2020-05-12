c25aed8b47ac82b035e4e973480d876020ea01c9
shin <cu7898@gmail.com>
2020-05-13 00:24:29 +0900

    TestP #115 booleanToString 테스트

--- Codewars_8kyu/quiz/N041N050/N043ConvertABooleanToAString/BooleanToString.test.js
+++ Codewars_8kyu/quiz/N041N050/N043ConvertABooleanToAString/BooleanToString.test.js
@@ -3,3 +3,7 @@ const booleanToString = require('./BooleanToString');
 test('booleanToString(true)은 "true"를 반환함',() =>{
     expect(booleanToString(true)).toBe('true');
 });
+
+test('booleanToString(false)은 "false"를 반환함',() =>{
+    expect(booleanToString(false)).toBe('false');
+});
