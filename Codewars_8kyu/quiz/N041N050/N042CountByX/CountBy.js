a7f883d7eb43a726c35569233e1a46c9df25103a
shin <cu7898@gmail.com>
2020-05-12 20:51:18 +0900

    Feat #113 countBy 테스트 통과

--- Codewars_8kyu/quiz/N041N050/N042CountByX/CountBy.js
+++ Codewars_8kyu/quiz/N041N050/N042CountByX/CountBy.js
@@ -1,5 +1,5 @@
 const countBy = function (interval, repeat) {
-    return [...Array(repeat).keys()].map(i => i + 1);
+    return [...Array(repeat).keys()].map(i => (i + 1) * interval);
 };
 
 module.exports = countBy;
