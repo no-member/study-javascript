975c29c83b48e7e7fa578da176771761319296eb
shin <cu7898@gmail.com>
2020-05-13 01:47:16 +0900

    TestP #117 cuboid.getVolumeOfCuboid 테스트

--- Codewars_8kyu/quiz/N041N050/N044VolumeOfACuboid/Cuboid.test.js
+++ Codewars_8kyu/quiz/N041N050/N044VolumeOfACuboid/Cuboid.test.js
@@ -3,3 +3,7 @@ const cuboid = require('./Cuboid');
 test('cuboid(1,2,2)은 4를 반환함',() =>{
     expect(cuboid.getVolumeOfCuboid(1,2,2)).toBe(4);
 });
+
+test('cuboid(6.4,2,5)은 64를 반환함',() =>{
+    expect(cuboid.getVolumeOfCuboid(6.4,2,5)).toBe(64);
+});
