b31ed9428824e08b794e7fd2bcb417d8313f7e43
shin <cu7898@gmail.com>
2020-05-13 01:46:08 +0900

    Refacotr #117

--- Codewars_8kyu/quiz/N041N050/N044VolumeOfACuboid/Cuboid.test.js
+++ Codewars_8kyu/quiz/N041N050/N044VolumeOfACuboid/Cuboid.test.js
@@ -1,6 +1,5 @@
 const cuboid = require('./Cuboid');
 
-test('cuboid(1,1)은 2를 반환함',() =>{
+test('cuboid(1,2,2)은 4를 반환함',() =>{
     expect(cuboid.getVolumeOfCuboid(1,2,2)).toBe(4);
-    console.log(cuboid);
 });
