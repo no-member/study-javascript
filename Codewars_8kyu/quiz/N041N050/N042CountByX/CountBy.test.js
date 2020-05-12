115f9e4b2cba33c91a06188f8f0eb6fe85032686
shin <cu7898@gmail.com>
2020-05-12 20:46:36 +0900

    TestF #113 countBy 테스트

--- Codewars_8kyu/quiz/N041N050/N042CountByX/CountBy.test.js
+++ Codewars_8kyu/quiz/N041N050/N042CountByX/CountBy.test.js
@@ -3,3 +3,7 @@ const countBy = require('./CountBy');
 test('countBy(1, 5)은 [1,2,3,4,5]를 반환함', () => {
     expect(countBy(1, 5)).toStrictEqual([1, 2, 3, 4, 5]);
 });
+
+test('countBy(1, 6)은 [1,2,3,4,5,6]를 반환함', () => {
+    expect(countBy(1, 6)).toStrictEqual([1, 2, 3, 4, 5, 6]);
+});
