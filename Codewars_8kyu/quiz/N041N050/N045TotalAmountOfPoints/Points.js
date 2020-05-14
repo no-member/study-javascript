3290bfdfb47520feb0050f128a705f85cbcde889
shin <cu7898@gmail.com>
2020-05-14 22:01:02 +0900

    Feat #119 sum 테스트 완료

--- Codewars_8kyu/quiz/N041N050/N045TotalAmountOfPoints/Points.js
+++ Codewars_8kyu/quiz/N041N050/N045TotalAmountOfPoints/Points.js
@@ -2,8 +2,21 @@ const Points = (function () {
     function Points() {
     }
 
-    Points.sum = function () {
-        return null;
+    Points.sum = function (games) {
+        return games.map(game => this.compare(game.split(':'))).reduce((x, y) => x + y, 0);
+    };
+
+    Points.compare = function (game) {
+        const firstNumber = game[0];
+        const secondNumber = game[1];
+
+        if (firstNumber > secondNumber) {
+            return 3;
+        }
+        if (firstNumber < secondNumber) {
+            return 0;
+        }
+        return 1;
     };
 
     return Points;
--- Codewars_8kyu/quiz/N041N050/N045TotalAmountOfPoints/Points.test.js
+++ Codewars_8kyu/quiz/N041N050/N045TotalAmountOfPoints/Points.test.js
@@ -1,5 +1,5 @@
 const Points = require('./Points');
 
-test('Points.sum(["0:1"])은 0를 반환함', () => {
-    expect(Points.sum(["0:1"])).toBe(0);
+test('Points.sum(["0:1", "2:3"])은 0를 반환함', () => {
+    expect(Points.sum(['0:1', '2:3'])).toBe(0);
 });
