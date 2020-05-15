1b99720ea9f2ec71dea956a6fd7c34cc6d22c2ca
shin <cu7898@gmail.com>
2020-05-15 19:50:25 +0900

    TestP #123 smash 테스트 통과

--- Codewars_8kyu/quiz/N041N050/N047SentenceSmash/SentenceSmash.test.js
+++ Codewars_8kyu/quiz/N041N050/N047SentenceSmash/SentenceSmash.test.js
@@ -7,3 +7,7 @@ test('sentenceSmash(["hello"])은 "hello"를 반환함', () => {
 test('sentenceSmash(["hello", "my", "friend"])은 "hello my friend"를 반환함', () => {
     expect(sentenceSmash(['hello', 'my', 'friend'])).toBe('hello my friend');
 });
+
+test('sentenceSmash([""])은 ""를 반환함', () => {
+    expect(sentenceSmash([''])).toBe('');
+});
