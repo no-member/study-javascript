1675d1f1e846da88ad892a84f045f02da6e55412
shin <cu7898@gmail.com>
2020-05-19 07:38:35 +0900

    TestP #125 betterThanAverage 테스트 케이스 추가

--- Codewars_8kyu/quiz/N041N050/N048HowGoodAreYouReally/Exam.test.js
+++ Codewars_8kyu/quiz/N041N050/N048HowGoodAreYouReally/Exam.test.js
@@ -7,3 +7,8 @@ test('betterThanAverage([2, 3], 5)은 true를 반환함', () => {
 test('betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)은 false 반환함', () => {
     expect(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)).toBe(false);
 });
+
+test('betterThanAverage([2, 4], 3)은 false 반환함', () => {
+    expect(betterThanAverage([2, 4], 3)).toBe(false);
+});
+
