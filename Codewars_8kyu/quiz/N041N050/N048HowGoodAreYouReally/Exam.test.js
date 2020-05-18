31143386aacfaa9907865ae9b3cf7efa037f402c
shin <cu7898@gmail.com>
2020-05-19 07:36:43 +0900

    TestP #125 betterThanAverage 테스트 통과

--- Codewars_8kyu/quiz/N041N050/N048HowGoodAreYouReally/Exam.test.js
+++ Codewars_8kyu/quiz/N041N050/N048HowGoodAreYouReally/Exam.test.js
@@ -3,3 +3,7 @@ const betterThanAverage = require('./Exam');
 test('betterThanAverage([2, 3], 5)은 true를 반환함', () => {
     expect(betterThanAverage([2, 3], 5)).toBe(true);
 });
+
+test('betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)은 false 반환함', () => {
+    expect(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 9)).toBe(false);
+});
