d58525950721a0675ce64681a93a3e1e3b2bb331
shin <cu7898@gmail.com>
2020-05-19 09:19:32 +0900

    TestP #127 calculateCost 테스트 - 숙박 일수 3일 미만

--- Codewars_8kyu/quiz/N041N050/N049TransportationOnVacation /RentCar.test.js
+++ Codewars_8kyu/quiz/N041N050/N049TransportationOnVacation /RentCar.test.js
@@ -1,5 +1,11 @@
 const calculateCost = require('./RentCar');
 
-test('calculateCost(1)은 40을 반환한다.', () => {
-    expect(calculateCost(1)).toBe(40);
+describe('숙박 일수가 3일 미만의 경우 테스트', () => {
+    test('calculateCost(1)은 40을 반환한다.', () => {
+        expect(calculateCost(1)).toBe(40);
+    });
+
+    test('calculateCost(2)은 80을 반환한다.', () => {
+        expect(calculateCost(2)).toBe(80);
+    });
 });
