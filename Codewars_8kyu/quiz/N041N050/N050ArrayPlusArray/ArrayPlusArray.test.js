c7b8700c1383ffbc6309fea70ffb268d62f57016
shin <cu7898@gmail.com>
2020-05-20 08:09:56 +0900

    TestP #129 arrayPlusArray 테스트 - 음수일 경우 테스트

--- Codewars_8kyu/quiz/N041N050/N050ArrayPlusArray/ArrayPlusArray.test.js
+++ Codewars_8kyu/quiz/N041N050/N050ArrayPlusArray/ArrayPlusArray.test.js
@@ -3,3 +3,7 @@ const arrayPlusArray = require('./ArrayPlusArray');
 test('arrayPlusArray([1, 2, 3], [4, 5, 6])은 21를 반환함', () => {
     expect(arrayPlusArray([1, 2, 3], [4, 5, 6])).toBe(21);
 });
+
+test('arrayPlusArray([-1, -2, -3], [-4, -5, -6])은 -21를 반환함', () => {
+    expect(arrayPlusArray([-1, -2, -3], [-4, -5, -6])).toBe(-21);
+});
