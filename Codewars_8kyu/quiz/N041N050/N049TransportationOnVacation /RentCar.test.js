ee6a6a3f5fd6f06352591fdcfa6bd3d969d339ee
shin <cu7898@gmail.com>
2020-05-19 09:20:40 +0900

    TestF #127 calculateCost 테스트 - 숙박 일수가 7일 미만

--- Codewars_8kyu/quiz/N041N050/N049TransportationOnVacation /RentCar.test.js
+++ Codewars_8kyu/quiz/N041N050/N049TransportationOnVacation /RentCar.test.js
@@ -9,3 +9,9 @@ describe('숙박 일수가 3일 미만의 경우 테스트', () => {
         expect(calculateCost(2)).toBe(80);
     });
 });
+
+describe('숙박 일수가 7일 미만의 경우 테스트', () => {
+    test('calculateCost(3)은 100을 반환한다.', () => {
+        expect(calculateCost(3)).toBe(100);
+    });
+});
