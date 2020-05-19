239c43d112d1908c19c306b60febd7af8ecf9d57
shin <cu7898@gmail.com>
2020-05-19 09:22:54 +0900

    Feat #127 calculateCost 테스트 통과

--- Codewars_8kyu/quiz/N041N050/N049TransportationOnVacation /RentCar.js
+++ Codewars_8kyu/quiz/N041N050/N049TransportationOnVacation /RentCar.js
@@ -1,7 +1,11 @@
 const DAY_PER_PAY = 40;
 
 const calculateCost = function (days) {
-    return DAY_PER_PAY * days;
+    const regularCost = DAY_PER_PAY * days;
+    if (days < 3) {
+        return regularCost;
+    }
+    return regularCost - 20;1
 };
 
 module.exports = calculateCost;
