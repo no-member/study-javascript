c6f57e14a469343ecdb5e40fd1dbf2a221ec52e0
shin <cu7898@gmail.com>
2020-05-20 08:13:41 +0900

    Final #129 arrayPlusArray 최종 정리

--- Codewars_8kyu/quiz/N041N050/N050ArrayPlusArray/ArrayPlusArray.js
+++ Codewars_8kyu/quiz/N041N050/N050ArrayPlusArray/ArrayPlusArray.js
@@ -3,7 +3,8 @@ const sumArray = function (array) {
 };
 
 const arrayPlusArray = function (array1, array2) {
-    return sumArray(array1) + sumArray(array2);
+    // return sumArray(array1) + sumArray(array2);
+    return sumArray(array1.concat(array2));
 };
 
 module.exports = arrayPlusArray;
