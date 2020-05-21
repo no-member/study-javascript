1360ae587b86006fa1f6cb9b47af75b9f7253893
shin <cu7898@gmail.com>
2020-05-22 08:21:36 +0900

    TestP #133 maskify 테스트 - 4글자 이하

--- Codewars_7kyu/quiz/N011N020/N012CreditCardMask/Maskify.test.js
+++ Codewars_7kyu/quiz/N011N020/N012CreditCardMask/Maskify.test.js
@@ -1,5 +1,9 @@
 const Maskify = require('./Maskify');
 
 test('maskify("1")은 "1"를 반환함', () => {
-    expect(Maskify.maskify("1")).toBe("1");
+    expect(Maskify.maskify('1')).toBe('1');
+});
+
+test('maskify("shin")은 "shin"를 반환함', () => {
+    expect(Maskify.maskify('shin')).toBe('shin');
 });
