f33c15b32ec92ecde489ceb502855f0dee768bd7
shin <cu7898@gmail.com>
2020-05-21 10:41:26 +0900

    Refactor #131 해결 방법 추가

--- Codewars_7kyu/quiz/N011N020/N011IsThisATriangle/Triangle.js
+++ Codewars_7kyu/quiz/N011N020/N011IsThisATriangle/Triangle.js
@@ -1,20 +1,22 @@
 class Triangle {
     static isTriangle(a, b, c) {
-        const triangle = [a, b, c];
+        // const triangle = [a, b, c];
+        //
+        // const longestSide = this.findLongest(triangle);
+        // const sumSideWithoutLongest = this.sumSide(triangle) - longestSide;
+        //
+        // return sumSideWithoutLongest > longestSide;
 
-        const longestSide = this.findLongest(triangle);
-        const sumSideWithoutLongest = this.sumSide(triangle) - longestSide;
-
-        return sumSideWithoutLongest > longestSide;
+        return a + b > c && a + c > b && b + c > a;
     }
 
-    static findLongest(triangle) {
-        return Math.max(...triangle);
-    }
-
-    static sumSide(triangle) {
-        return triangle.reduce((a, b) => a + b, 0);
-    }
+    // static findLongest(triangle) {
+    //     return Math.max(...triangle);
+    // }
+    //
+    // static sumSide(triangle) {
+    //     return triangle.reduce((a, b) => a + b, 0);
+    // }
 }
 
 module.exports = Triangle;
