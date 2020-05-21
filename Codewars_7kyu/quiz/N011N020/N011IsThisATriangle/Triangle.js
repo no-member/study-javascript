410100c386e4d26f9466a7de5c9ef93c863dbe67
shin <cu7898@gmail.com>
2020-05-21 10:39:40 +0900

    Feat #131 isTriangle 테스트 통과

--- Codewars_7kyu/quiz/N011N020/N011IsThisATriangle/Triangle.js
+++ Codewars_7kyu/quiz/N011N020/N011IsThisATriangle/Triangle.js
@@ -1,6 +1,19 @@
 class Triangle {
     static isTriangle(a, b, c) {
-        return true;
+        const triangle = [a, b, c];
+
+        const longestSide = this.findLongest(triangle);
+        const sumSideWithoutLongest = this.sumSide(triangle) - longestSide;
+
+        return sumSideWithoutLongest > longestSide;
+    }
+
+    static findLongest(triangle) {
+        return Math.max(...triangle);
+    }
+
+    static sumSide(triangle) {
+        return triangle.reduce((a, b) => a + b, 0);
     }
 }
 
