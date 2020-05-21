1de63d7793cfc60409cf95a16524b52887ce0e5b
shin <cu7898@gmail.com>
2020-05-22 08:26:01 +0900

    Feat #133 modify 테스트 통과

--- Codewars_7kyu/quiz/N011N020/N012CreditCardMask/Maskify.js
+++ Codewars_7kyu/quiz/N011N020/N012CreditCardMask/Maskify.js
@@ -1,5 +1,8 @@
 class Maskify {
     static maskify(secret) {
+        if (secret.length > 4) {
+            return "#".repeat(secret.length - 4) + secret.slice(secret.length - 4);
+        }
         return secret
     }
 }
