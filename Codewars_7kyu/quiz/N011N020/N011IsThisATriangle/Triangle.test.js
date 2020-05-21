b3d4afc4511d62ea933ec9d138012432c0aed408
shin <cu7898@gmail.com>
2020-05-21 10:20:50 +0900

    TestF #131 isTriangle 테스트

--- Codewars_7kyu/quiz/N011N020/N011IsThisATriangle/Triangle.test.js
+++ Codewars_7kyu/quiz/N011N020/N011IsThisATriangle/Triangle.test.js
@@ -3,3 +3,7 @@ const Triangle = require('./Triangle');
 test('isTriangle(1, 2, 2)은 true를 반환함', () => {
     expect(Triangle.isTriangle(1, 2, 2)).toBe(true);
 });
+
+test('isTriangle(2, 2, 4)은 false를 반환함', () => {
+    expect(Triangle.isTriangle(2, 2, 4)).toBe(false);
+});
