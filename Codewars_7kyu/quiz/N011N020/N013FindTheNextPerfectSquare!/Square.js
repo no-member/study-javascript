44a2448058b40095eeaf38f2bd59fc25b591fe10
shin <cu7898@gmail.com>
2020-05-25 08:21:28 +0900

    Final #135 findNextSquare 최종 정리

--- Codewars_7kyu/quiz/N011N020/N013FindTheNextPerfectSquare!/Square.js
+++ Codewars_7kyu/quiz/N011N020/N013FindTheNextPerfectSquare!/Square.js
@@ -1,7 +1,8 @@
 class Square {
     static findNextSquare(num) {
         const squareNumber = Math.sqrt(num);
-        return Math.floor(squareNumber) === squareNumber ? Math.pow((squareNumber + 1), 2) : -1;
+        // return Math.floor(squareNumber) === squareNumber ? Math.pow((squareNumber + 1), 2) : -1;
+        return squareNumber % 1 ? -1 : Math.pow((squareNumber + 1), 2)
     }
 }
 
