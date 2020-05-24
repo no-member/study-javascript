c85c19a55a7164a29c5f41958c9071d3cf8193f3
shin <cu7898@gmail.com>
2020-05-25 08:07:16 +0900

    Refactore #135

--- Codewars_7kyu/quiz/N011N020/N013FindTheNextPerfectSquare!/FindNextSquare.test.js
+++ Codewars_7kyu/quiz/N011N020/N013FindTheNextPerfectSquare!/FindNextSquare.test.js
@@ -1,13 +1,15 @@
 const Square = require('./Square');
 
-test('findNextSquare(1)은 4를 반환함', () => {
-    expect(Square.findNextSquare(1)).toBe(4);
-});
+describe('should return the next square for perfect squares', () => {
+    test('findNextSquare(1)은 4를 반환함', () => {
+        expect(Square.findNextSquare(1)).toBe(4);
+    });
 
-test('findNextSquare(4)은 9를 반환함', () => {
-    expect(Square.findNextSquare(4)).toBe(9);
-});
+    test('findNextSquare(4)은 9를 반환함', () => {
+        expect(Square.findNextSquare(4)).toBe(9);
+    });
 
-test('findNextSquare(15241383936)은 15241630849를 반환함', () => {
-    expect(Square.findNextSquare(15241383936)).toBe(15241630849);
+    test('findNextSquare(15241383936)은 15241630849를 반환함', () => {
+        expect(Square.findNextSquare(15241383936)).toBe(15241630849);
+    });
 });
