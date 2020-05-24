821b4b4b8598c6098413d554093e0f083cc232f4
shin <cu7898@gmail.com>
2020-05-25 08:47:53 +0900

    TestP #137 longest 테스트 통과

--- Codewars_7kyu/quiz/N011N020/N014TwoToOne/TwoToOne.test.js
+++ Codewars_7kyu/quiz/N011N020/N014TwoToOne/TwoToOne.test.js
@@ -3,3 +3,7 @@ const TwoToOne = require('./TwoToOne');
 test('TwoToOne.longest("aretheyhere","yestheyarehere")은 "aehrsty"를 반환함', () => {
     expect(TwoToOne.longest('aretheyhere', 'yestheyarehere')).toBe('aehrsty');
 });
+
+test('TwoToOne.longest("loopingisfunbutdangerous","lessdangerousthancoding")은 "abcdefghilnoprstu"를 반환함', () => {
+    expect(TwoToOne.longest('loopingisfunbutdangerous', 'lessdangerousthancoding')).toBe('abcdefghilnoprstu');
+});
