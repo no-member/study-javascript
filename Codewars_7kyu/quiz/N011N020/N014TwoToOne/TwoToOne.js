28873122d41cdacce5281259a8f96ec02f25e9ed
shin <cu7898@gmail.com>
2020-05-25 08:52:39 +0900

    Final #137 longest 최종 정리

--- Codewars_7kyu/quiz/N011N020/N014TwoToOne/TwoToOne.js
+++ Codewars_7kyu/quiz/N011N020/N014TwoToOne/TwoToOne.js
@@ -1,12 +1,13 @@
 class TwoToOne {
     static longest(str1, str2) {
-        const array = [...(str1 + str2)];
-        return this.removeDuplicates(array).sort().join('');
+        // const array = [...(str1 + str2)];
+        // return this.removeDuplicates(array).sort().join('');
+        return [...new Set(str1 + str2)].sort().join('');
     }
 
-    static removeDuplicates(array) {
-        return array.filter((a, b) => array.indexOf(a) === b);
-    }
+    // static removeDuplicates(array) {
+    //     return array.filter((a, b) => array.indexOf(a) === b);
+    // }
 }
 
 module.exports = TwoToOne;
