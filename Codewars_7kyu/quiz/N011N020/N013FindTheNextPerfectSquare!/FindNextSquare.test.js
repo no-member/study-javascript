b53300cd567048226b28eb50c17b8c619b7b4b26
shin <cu7898@gmail.com>
2020-05-25 07:53:58 +0900

    TestF #135 findNextSquare 테스트

--- Codewars_7kyu/quiz/N011N020/N013FindTheNextPerfectSquare!/FindNextSquare.test.js
+++ Codewars_7kyu/quiz/N011N020/N013FindTheNextPerfectSquare!/FindNextSquare.test.js
@@ -3,3 +3,7 @@ const Square = require('./Square');
 test('findNextSquare(1)은 4를 반환함', () => {
     expect(Square.findNextSquare(1)).toBe(4);
 });
+
+test('findNextSquare(4)은 9를 반환함', () => {
+    expect(Square.findNextSquare(4)).toBe(9);
+});
