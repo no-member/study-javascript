3a434057ab80188a6ee4ac4165408e6b234774cf
shin <cu7898@gmail.com>
2020-05-26 08:15:17 +0900

    Refactor #139

--- Codewars_7kyu/quiz/N011N020/N015RegexValidatePINCode/ValidatePIN.test.js
+++ Codewars_7kyu/quiz/N011N020/N015RegexValidatePINCode/ValidatePIN.test.js
@@ -1,9 +1,13 @@
 const ValidatePIN = require('./ValidatePIN');
 
-test('ValidatePIN.validatePin("1234")은 true 반환함', () => {
-    expect(ValidatePIN.validatePin("1234")).toBe(true);
+describe('when pin contain characters return false', () => {
+    test('ValidatePIN.validatePin("1234")은 true 반환함', () => {
+        expect(ValidatePIN.validatePin("1234")).toBe(true);
+    });
 });
 
-test('ValidatePIN.validatePin("123a")은 false 반환함', () => {
-    expect(ValidatePIN.validatePin("123a˙")).toBe(false);
+describe('when pass pin validate return true', () => {
+    test('ValidatePIN.validatePin("123a")은 false 반환함', () => {
+        expect(ValidatePIN.validatePin("123a˙")).toBe(false);
+    });
 });
