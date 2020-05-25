54476ba9ad7a43e73ef274f78101a71dca4100d9
shin <cu7898@gmail.com>
2020-05-26 08:17:17 +0900

    TestF #139 validatePin 테스트

--- Codewars_7kyu/quiz/N011N020/N015RegexValidatePINCode/ValidatePIN.test.js
+++ Codewars_7kyu/quiz/N011N020/N015RegexValidatePINCode/ValidatePIN.test.js
@@ -4,10 +4,18 @@ describe('when pin contain characters return false', () => {
     test('ValidatePIN.validatePin("1234")은 true 반환함', () => {
         expect(ValidatePIN.validatePin("1234")).toBe(true);
     });
+
+    test('ValidatePIN.validatePin("012345")은 true 반환함', () => {
+        expect(ValidatePIN.validatePin("012345")).toBe(true);
+    });
 });
 
 describe('when pass pin validate return true', () => {
     test('ValidatePIN.validatePin("123a")은 false 반환함', () => {
         expect(ValidatePIN.validatePin("123a˙")).toBe(false);
+    });
+
+    test('ValidatePIN.validatePin("1A3400")은 false 반환함', () => {
+        expect(ValidatePIN.validatePin("1A3400")).toBe(false);
     });
 });
