acd8705ef1705eef04cedbfc0393b4785cec72b4
shin <cu7898@gmail.com>
2020-05-28 08:24:55 +0900

    Feat #143 add 테스트 통과

--- Codewars_7kyu/quiz/N011N020/N017BinaryAddition/Binary.js
+++ Codewars_7kyu/quiz/N011N020/N017BinaryAddition/Binary.js
@@ -1,6 +1,18 @@
 class Binary {
     static add(num1, num2) {
-        return '10'
+        return this.numToBinary(num1 + num2);
+    }
+
+    static numToBinary(num) {
+        let binary = '';
+        while (num > 0) {
+            const remainder = num % 2;
+            binary = remainder + binary;
+
+            num = Math.floor(num / 2);
+        }
+
+        return binary;
     }
 }
 
