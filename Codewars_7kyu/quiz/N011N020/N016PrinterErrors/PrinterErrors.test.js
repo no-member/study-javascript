9391f7c240acf501945ef5d84cc0c819da3d8c50
shin <cu7898@gmail.com>
2020-05-27 09:07:05 +0900

    TestP #141 printerError 추가 테스트

--- Codewars_7kyu/quiz/N011N020/N016PrinterErrors/PrinterErrors.test.js
+++ Codewars_7kyu/quiz/N011N020/N016PrinterErrors/PrinterErrors.test.js
@@ -4,3 +4,8 @@ test('PrinterErrors(s)은 "3/56"를 반환함', () => {
     const s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
     expect(PrinterErrors.printerErrors(s)).toBe('3/56');
 });
+
+test('PrinterErrors(s)은 "1/3"를 반환함', () => {
+    const s = "aez";
+    expect(PrinterErrors.printerErrors(s)).toBe('1/3');
+});
