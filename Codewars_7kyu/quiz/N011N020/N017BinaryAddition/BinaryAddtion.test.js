5865be4d8cde9e937ae2f581033df2a16d9cd651
shin <cu7898@gmail.com>
2020-05-28 08:08:29 +0900

    TestF #143 add 테스트

--- Codewars_7kyu/quiz/N011N020/N017BinaryAddition/BinaryAddtion.test.js
+++ Codewars_7kyu/quiz/N011N020/N017BinaryAddition/BinaryAddtion.test.js
@@ -3,3 +3,7 @@ const Binary = require('./Binary');
 test('add(1, 1)은 "10"를 반환함', () => {
     expect(Binary.add(1, 1)).toBe('10');
 });
+
+test('add(2, 1)은 "11"를 반환함', () => {
+    expect(Binary.add(2, 1)).toBe('11');
+});
