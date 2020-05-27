b41b36c8584ebb4a9c6ed8761d0e36ab35ca0159
shin <cu7898@gmail.com>
2020-05-27 09:09:49 +0900

    Final #141 printErrors 최종 정리

--- Codewars_7kyu/quiz/N011N020/N016PrinterErrors/PrinterErrors.js
+++ Codewars_7kyu/quiz/N011N020/N016PrinterErrors/PrinterErrors.js
@@ -1,6 +1,8 @@
 class PrinterErrors {
     static printerErrors(s) {
-        return s.replace(/[a-m]/g, '').length + '/' + s.length;
+        // return s.replace(/[a-m]/g, '').length + '/' + s.length;
+
+        return s.match(/[^a-m]/g).length + '/' + s.length;
     }
 }
 
