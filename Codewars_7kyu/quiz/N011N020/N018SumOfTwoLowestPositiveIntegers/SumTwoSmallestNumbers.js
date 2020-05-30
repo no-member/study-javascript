0ac045210c6c5b860e0c66c7ef887635de561bb3
shin <cu7898@gmail.com>
2020-05-30 13:15:05 +0900

    Refactor #145 로직을 명확하게 변경

--- Codewars_7kyu/quiz/N011N020/N018SumOfTwoLowestPositiveIntegers/SumTwoSmallestNumbers.js
+++ Codewars_7kyu/quiz/N011N020/N018SumOfTwoLowestPositiveIntegers/SumTwoSmallestNumbers.js
@@ -1,6 +1,14 @@
 class SumTwoSmallestNumbers {
     static sumTwoSmallestNumbers(nums) {
-        return 13;
+        nums.sort(function (a, b) {
+            return a - b;
+        });
+
+        return this.sumCountNumbers(2, nums);
+    }
+
+    static sumCountNumbers(count, numbers) {
+        return numbers.slice(0, count).reduce((x, y) => x + y);
     }
 }
 
--- Codewars_7kyu/quiz/N011N020/N018SumOfTwoLowestPositiveIntegers/SumTwoSmallestNumbers.test.js
+++ Codewars_7kyu/quiz/N011N020/N018SumOfTwoLowestPositiveIntegers/SumTwoSmallestNumbers.test.js
@@ -5,5 +5,5 @@ test('sumTwoSmallestNumbers([5, 8, 12, 19, 22])은 "13"를 반환함', () => {
 });
 
 test('sumTwoSmallestNumbers([15, 28, 4, 2, 43])은 "6"를 반환함', () => {
-    expect(SumTwoSmallestNumbers.sumTwoSmallestNumbers([[15, 28, 4, 2, 43]])).toBe(6);
+    expect(SumTwoSmallestNumbers.sumTwoSmallestNumbers([15, 28, 4, 2, 43])).toBe(6);
 });
