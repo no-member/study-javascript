97db0d69fd7484666667a5669b438dc1281849fa
shin <cu7898@gmail.com>
2020-06-05 07:44:30 +0900

    TestP #151 findDivisors 테스트 케이스 추가

--- Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.test.js
+++ Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.test.js
@@ -6,7 +6,6 @@ describe('divisors return array', () => {
             .toStrictEqual([2]);
     });
 
-
     test('test2', () => {
         expect(Divisors.findDivisor(6))
             .toStrictEqual([2, 3]);
@@ -22,5 +21,10 @@ describe('divisors return string', () => {
     test('test2', () => {
         expect(Divisors.findDivisor(5))
             .toStrictEqual('5 is prime');
+    });
+
+    test('test2', () => {
+        expect(Divisors.findDivisor(1))
+            .toStrictEqual('1 is prime');
     });
 })
