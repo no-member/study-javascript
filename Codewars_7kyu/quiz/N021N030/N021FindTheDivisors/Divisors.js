e81fe24565f9cbe8f2b75ab2f4d5f0d5aef87882
shin <cu7898@gmail.com>
2020-06-05 07:29:43 +0900

    TestF #151 findDivisor 테스트

--- Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.js
+++ Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.js
@@ -1,5 +1,5 @@
 class Divisors {
-    static FindDivisor(n) {
+    static findDivisor(n) {
         const range2ToN = [...Array(n).keys()].filter(x => x>1)
         const divisors = range2ToN.filter(x => n%x === 0)
 
--- Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.test.js
+++ Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.test.js
@@ -2,20 +2,25 @@ const Divisors = require('./Divisors');
 
 describe('divisors return array', () => {
     test('test1', () => {
-        expect(Divisors.FindDivisor(4))
+        expect(Divisors.findDivisor(4))
             .toStrictEqual([2]);
     });
 
 
     test('test2', () => {
-        expect(Divisors.FindDivisor(6))
+        expect(Divisors.findDivisor(6))
             .toStrictEqual([2, 3]);
     });
 });
 
 describe('divisors return string', () => {
     test('test1', () => {
-        expect(Divisors.FindDivisor(2))
+        expect(Divisors.findDivisor(2))
             .toStrictEqual('2 is prime');
+    });
+
+    test('test2', () => {
+        expect(Divisors.findDivisor(5))
+            .toStrictEqual('5 is prime');
     });
 })
