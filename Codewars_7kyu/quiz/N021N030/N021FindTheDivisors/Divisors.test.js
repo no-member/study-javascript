be53bc2b3fbfef3525abee9ce13f19cfa0888f3a
shin <cu7898@gmail.com>
2020-06-05 05:25:45 +0900

    TestF #151 divisors 테스트

--- Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.test.js
+++ Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.test.js
@@ -1,7 +1,12 @@
 const Divisors = require('./Divisors');
 
-test('divisors test1', () => {
+test('divisors return array test1', () => {
     expect(Divisors.divisors(4))
         .toStrictEqual([2]);
 });
 
+
+test('divisors return array test2', () => {
+    expect(Divisors.divisors(6))
+        .toStrictEqual([2, 3]);
+});
