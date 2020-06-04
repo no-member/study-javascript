675a39b315c04048129010853b37968815d0e74b
shin <cu7898@gmail.com>
2020-06-05 07:31:46 +0900

    Refatctor #151 findDivisors 로직 분할

--- Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.js
+++ Codewars_7kyu/quiz/N021N030/N021FindTheDivisors/Divisors.js
@@ -3,10 +3,11 @@ class Divisors {
         const range2ToN = [...Array(n).keys()].filter(x => x>1)
         const divisors = range2ToN.filter(x => n%x === 0)
 
-        if (divisors.length === 0){
-            return  `${n} is prime`
-        }
-        return divisors;
+        return this.isBlank(divisors) ? `${n} is prime` : divisors;
+    }
+
+    static isBlank(array) {
+        return array.length === 0;
     }
 }
 
