976a062dbb5e72d449812bac0654e381a29184cc
shin <cu7898@gmail.com>
2020-06-06 12:35:54 +0900

    Refactor #153 countPassenger

--- Codewars_7kyu/quiz/N021N030/N022NumberOfPeopleInTheBus/NumberOfPeopleInTheBus.test.js
+++ Codewars_7kyu/quiz/N021N030/N022NumberOfPeopleInTheBus/NumberOfPeopleInTheBus.test.js
@@ -1,6 +1,6 @@
 const Divisors = require('./NumberOfPeopleInTheBus');
 
-describe('#count test', () => {
+describe('#countPassenger test => bus stop just one', () => {
     test('test1', () => {
         expect(Divisors.countPassenger([[10,0]])).toStrictEqual(10);
     });
