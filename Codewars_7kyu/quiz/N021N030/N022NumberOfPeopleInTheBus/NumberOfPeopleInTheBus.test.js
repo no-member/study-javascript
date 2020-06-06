3fb1195f8805d71a973b3f0368dda2827a90f0eb
shin <cu7898@gmail.com>
2020-06-06 12:39:11 +0900

    TestF #153 countPassenger 테스트

--- Codewars_7kyu/quiz/N021N030/N022NumberOfPeopleInTheBus/NumberOfPeopleInTheBus.test.js
+++ Codewars_7kyu/quiz/N021N030/N022NumberOfPeopleInTheBus/NumberOfPeopleInTheBus.test.js
@@ -4,5 +4,9 @@ describe('#countPassenger test => bus stop just one', () => {
     test('test1', () => {
         expect(Divisors.countPassenger([[10,0]])).toStrictEqual(10);
     });
+
+    test('test2', () => {
+        expect(Divisors.countPassenger([[3,1]])).toStrictEqual(2);
+    });
 });
 
