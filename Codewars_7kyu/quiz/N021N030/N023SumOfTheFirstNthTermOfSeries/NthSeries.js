aee536cd4d5e356821965584ffdfff6a7e5e9727
takamizawa <cu7898@gmail.com>
2020-06-08 07:08:16 +0900

    Feat #157 seriesSum 테스트 통과

--- Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.js
+++ Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.js
@@ -1,6 +1,11 @@
 class NthSeries {
     static seriesSum(number) {
-        return "1.00"
+        let sum = 0;
+        for (let i = 0; i < number; i++) {
+            sum += Math.round((1 / (i * 3 + 1)) * 100) / 100
+        }
+
+        return sum.toFixed(2).toString();
     }
 }
 
