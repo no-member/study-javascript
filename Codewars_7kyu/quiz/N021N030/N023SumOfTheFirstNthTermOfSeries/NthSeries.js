a39f2ddd6fe34d8993c492c0d59f8758555660e6
takamizawa <cu7898@gmail.com>
2020-06-08 07:21:30 +0900

    Refactor #157 seriesSum 로직 변경 (타인)

--- Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.js
+++ Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.js
@@ -2,10 +2,11 @@ class NthSeries {
     static seriesSum(number) {
         let sum = 0;
         for (let i = 0; i < number; i++) {
-            sum += Math.round((1 / (i * 3 + 1)) * 100) / 100
+            sum += 1 / (i * 3 + 1);
         }
 
-        return sum.toFixed(2).toString();
+        // return sum.toFixed(2).toString();
+        return sum.toFixed(2);
     }
 }
 
--- Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.test.js
+++ Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.test.js
@@ -8,5 +8,9 @@ describe('#seriseSum test', () => {
     test('test2', () => {
         expect(NthSeries.seriesSum(2)).toStrictEqual("1.25");
     });
+
+    test('test3', () => {
+        expect(NthSeries.seriesSum(3)).toStrictEqual("1.39");
+    });
 });
 
