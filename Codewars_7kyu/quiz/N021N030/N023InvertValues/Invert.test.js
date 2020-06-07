b77f1c7ceb8a9246715fd2cf893773a3cc2e6383
shin <cu7898@gmail.com>
2020-06-08 00:26:31 +0900

    TestP #155 invert 테스트 -> 음수를 추가

--- Codewars_7kyu/quiz/N021N030/N023InvertValues/Invert.test.js
+++ Codewars_7kyu/quiz/N021N030/N023InvertValues/Invert.test.js
@@ -5,8 +5,13 @@ describe('#invert test', () => {
         expect(Invert.invert([1])).toStrictEqual([-1]);
     });
 
-    test('test1', () => {
-        expect(Invert.invert([1, 3, 5])).toStrictEqual([-1, -3, - 5]);
+    test('test2', () => {
+        expect(Invert.invert([1, 3, 5])).toStrictEqual([-1, -3, -5]);
     });
+
+    test('test3', () => {
+        expect(Invert.invert([1, 3, -5])).toStrictEqual([-1, -3, 5]);
+    });
+
 });
 
