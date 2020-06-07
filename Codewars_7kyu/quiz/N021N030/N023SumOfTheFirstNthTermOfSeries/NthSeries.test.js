7237aef051da59834021388b18603f318abfae62
takamizawa <cu7898@gmail.com>
2020-06-08 06:57:10 +0900

    TestF #157 seriesSum 테스트

--- Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.test.js
+++ Codewars_7kyu/quiz/N021N030/N023SumOfTheFirstNthTermOfSeries/NthSeries.test.js
@@ -4,5 +4,9 @@ describe('#seriseSum test', () => {
     test('test1', () => {
         expect(NthSeries.seriesSum(1)).toStrictEqual("1.00");
     });
+
+    test('test2', () => {
+        expect(NthSeries.seriesSum(2)).toStrictEqual("1.25");
+    });
 });
 
