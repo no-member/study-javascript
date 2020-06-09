be3b9b346800897298c8b5eff8660fe3ce9a55fa
takamizawa <cu7898@gmail.com>
2020-06-09 13:32:03 +0900

    TestF #159 removeSmallest 테스트

--- Codewars_7kyu/quiz/N021N030/N024RemoveTheMinimum/RemoveSmallest.test.js
+++ Codewars_7kyu/quiz/N021N030/N024RemoveTheMinimum/RemoveSmallest.test.js
@@ -1,8 +1,12 @@
 const RemoveSmallest = require('./RemoveSmallest');
 
-describe('#removeSmallest normal', () => {
+describe('#removeSmallest basic tests', () => {
     test('test1', () => {
         expect(RemoveSmallest.removeSmallest([1, 2])).toStrictEqual([2]);
+    });
+
+    test('test2', () => {
+        expect(RemoveSmallest.removeSmallest([1, 2, 4])).toStrictEqual([2, 4]);
     });
 });
 
