90af1382576b813b1b2e3df2f381aa98347d5554
takamizawa <cu7898@gmail.com>
2020-06-10 07:55:05 +0900

    TestP #161 isEndWith 테스트 통과

--- Codewars_7kyu/quiz/N021N030/N025StringEndsWith?/MyStringUtils.test.js
+++ Codewars_7kyu/quiz/N021N030/N025StringEndsWith?/MyStringUtils.test.js
@@ -4,10 +4,18 @@ describe('#isEndWith return true tests', () => {
     test('test1', () => {
         expect(MyStringUtils.isEndWith('abc', 'bc')).toStrictEqual(true);
     });
+
+    test('test2', () => {
+        expect(MyStringUtils.isEndWith('abc123', 'c123')).toStrictEqual(true);
+    });
 });
 
 describe('#isEndWith return false tests', () => {
     test('test1', () => {
         expect(MyStringUtils.isEndWith('abc', 'd')).toStrictEqual(false);
+    });
+
+    test('test2', () => {
+        expect(MyStringUtils.isEndWith('abc123', 'c')).toStrictEqual(false);
     });
 });
