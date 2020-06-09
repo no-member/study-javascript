cd4892c3905a19a4de2fa753305640fcd82f6cf8
takamizawa <cu7898@gmail.com>
2020-06-09 13:53:48 +0900

    TestP #159 removeSmallest 테스트 케이스 추가

--- Codewars_7kyu/quiz/N021N030/N024RemoveTheMinimum/RemoveSmallest.test.js
+++ Codewars_7kyu/quiz/N021N030/N024RemoveTheMinimum/RemoveSmallest.test.js
@@ -8,5 +8,13 @@ describe('#removeSmallest basic tests', () => {
     test('test2', () => {
         expect(RemoveSmallest.removeSmallest([1, 2, 4])).toStrictEqual([2, 4]);
     });
+
+    test('test2', () => {
+        expect(RemoveSmallest.removeSmallest([2, 4, 10, 4])).toStrictEqual([4, 10 , 4]);
+    });
+
+    test('test2', () => {
+        expect(RemoveSmallest.removeSmallest([3, 0, 5 , 3])).toStrictEqual([3, 5, 3]);
+    });
 });
 
