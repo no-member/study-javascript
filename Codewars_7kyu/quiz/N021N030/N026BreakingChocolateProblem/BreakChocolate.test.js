50ec959b4d909dcd7943da2d4be6238113785980
takamizawa <cu7898@gmail.com>
2020-06-11 08:01:54 +0900

    TestF #163 breakChocolate 테스트

--- Codewars_7kyu/quiz/N021N030/N026BreakingChocolateProblem/BreakChocolate.test.js
+++ Codewars_7kyu/quiz/N021N030/N026BreakingChocolateProblem/BreakChocolate.test.js
@@ -4,4 +4,8 @@ describe('#breakChocolate tests', () => {
     test('test1', () => {
         expect(BreakChocolate.breakChocolate(1, 1)).toStrictEqual(0);
     });
+
+    test('test2', () => {
+        expect(BreakChocolate.breakChocolate(2, 1)).toStrictEqual(1);
+    });
 });
