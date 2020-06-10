47ad753892f6e59a9124a5c9c4f65634ca998137
takamizawa <cu7898@gmail.com>
2020-06-11 08:22:23 +0900

    Feat #163 breakChocolate pass test

--- Codewars_7kyu/quiz/N021N030/N026BreakingChocolateProblem/BreakChocolate.js
+++ Codewars_7kyu/quiz/N021N030/N026BreakingChocolateProblem/BreakChocolate.js
@@ -1,6 +1,6 @@
 class BreakChocolate {
     static breakChocolate(word, ending) {
-        if (word === 0) {
+        if (word === 0 || ending === 0) {
             return 0;
         }
         return word * ending - 1;
