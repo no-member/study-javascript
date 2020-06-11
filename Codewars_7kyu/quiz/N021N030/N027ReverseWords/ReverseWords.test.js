425484e6fd96456f880b773d73af1ed8a36a609d
takamizawa <cu7898@gmail.com>
2020-06-12 07:42:23 +0900

    TestF #166 reverse test

--- Codewars_7kyu/quiz/N021N030/N027ReverseWords/ReverseWords.test.js
+++ Codewars_7kyu/quiz/N021N030/N027ReverseWords/ReverseWords.test.js
@@ -2,8 +2,12 @@ const ReverseWords = require('./ReverseWords');
 
 describe('#reverse', () => {
     describe('with a one word', () => {
-        it('should return reversed word', function () {
+        it('should return reversed word test1', function () {
             expect(ReverseWords.reverse('hi')).toStrictEqual('ih');
+        });
+
+        it('should return reversed word test2', function () {
+            expect(ReverseWords.reverse('word')).toStrictEqual('drow');
         });
     });
 });
