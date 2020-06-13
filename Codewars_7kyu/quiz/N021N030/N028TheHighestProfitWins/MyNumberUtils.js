11a3a40e087a0db9e1a97aa5ef5d525dc329273e
shin <cu7898@gmail.com>
2020-06-13 15:30:05 +0900

    Feat #168 minMax

---
+++ Codewars_7kyu/quiz/N021N030/N028TheHighestProfitWins/MyNumberUtils.js
@@ -0,0 +1,10 @@
+class MyNumberUtils {
+    static minMax(numbers) {
+        const min = Math.min(...numbers);
+        const max = Math.max(...numbers);
+
+        return [min, max];
+    }
+}
+
+module.exports = MyNumberUtils;
