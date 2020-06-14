391cddc02a378aaedeec440b3f8c03d843ffe65e
shin <cu7898@gmail.com>
2020-06-15 00:33:09 +0900

    Feat #170 sumIsOddOrEven

---
+++ Codewars_7kyu/quiz/N021N030/N029OddOrEven/MyNumberUtils.js
@@ -0,0 +1,16 @@
+class MyNumberUtils {
+    static sumIsOddOrEven(arrayNumber) {
+        return this.isOddOrEven(this.sum(arrayNumber));
+    }
+
+    static sum(array) {
+        return array.reduce((x, y) => x + y, 0);
+    }
+
+    static isOddOrEven(number) {
+        return number % 2 === 0 ? 'even' : 'odd'
+    }
+}
+
+
+module.exports = MyNumberUtils;
---
+++ Codewars_7kyu/quiz/N021N030/N029OddOrEven/sumIsOddOrEven.spec.js
@@ -0,0 +1,49 @@
+const MyNumberUtils = require('./MyNumberUtils')
+
+describe('edge test', () => {
+    describe('with []', () => {
+        it('return even', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([])).toStrictEqual('even');
+        });
+    });
+
+    describe('with [0]', () => {
+        it('return even', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([0])).toStrictEqual('even');
+        });
+    });
+
+    describe('with [1]', () => {
+        it('return odd', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([1])).toStrictEqual('odd');
+        });
+    });
+});
+
+describe('even test', () => {
+    describe('with [0, 1, 5]', () => {
+        it('return even', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([0, 1, 5])).toStrictEqual('even');
+        });
+    });
+
+    describe('with [0, 1, 3]', () => {
+        it('return even', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([0, 1, 3])).toStrictEqual('even');
+        });
+    });
+})
+
+describe('odd test', () => {
+    describe('with [0, 1, 6]', () => {
+        it('return odd', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([0, 1, 6])).toStrictEqual('odd');
+        });
+    });
+
+    describe('with [0, 1, 4]', () => {
+        it('return odd', function () {
+            expect(MyNumberUtils.sumIsOddOrEven([0, 1, 4])).toStrictEqual('odd');
+        });
+    });
+})
