81ff1bd02a13b0329dd2b45c086f2aa0e847aa62
takamizawa <cu7898@gmail.com>
2020-06-15 20:11:59 +0900

    Refactor #172 find change logic

--- Codewars_7kyu/quiz/N021N030/N030FindTheStrayNumber/Stray.js
+++ Codewars_7kyu/quiz/N021N030/N030FindTheStrayNumber/Stray.js
@@ -1,8 +1,13 @@
 class Stray {
+    // static find(numbers) {
+    //     return numbers.find(
+    //         number => numbers.indexOf(number) === numbers.lastIndexOf(number)
+    //     )
+    // }
+
     static find(numbers) {
-        return numbers.find(
-            number => numbers.indexOf(number) === numbers.lastIndexOf(number)
-        )
+        numbers.sort();
+        return numbers.filter(n => n === numbers[0]).length === 1 ? numbers[0] : numbers[numbers.length - 1];
     }
 }
 
