41a2aee470fd0e77e87bb38e4acdde77e2928a16
shin <cu7898@gmail.com>
2020-07-09 22:45:23 +0900

    Finish Triangular Treasure

--- Codewars_7kyu/quiz/N031N040/N032TriangularTreasure/triangular.test.js
+++ Codewars_7kyu/quiz/N031N040/N032TriangularTreasure/triangular.test.js
@@ -1,6 +1,12 @@
 const MyNumberUtils = require('./MyNumberUtils');
 
 describe('triangular', () => {
+    describe('with -1', () => {
+        it('return -1', () => {
+            expect(MyNumberUtils.triangular(-1)).toStrictEqual(0);
+        });
+    });
+
     describe('with 0', () => {
         it('return 0', () => {
             expect(MyNumberUtils.triangular(0)).toStrictEqual(0);
@@ -10,6 +16,12 @@ describe('triangular', () => {
     describe('with 1', () => {
         it('return 1', () => {
             expect(MyNumberUtils.triangular(1)).toStrictEqual(1);
+        });
+    });
+
+    describe('with 3', () => {
+        it('return 6', () => {
+            expect(MyNumberUtils.triangular(3)).toStrictEqual(6);
         });
     });
 });
