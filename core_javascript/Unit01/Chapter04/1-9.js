8a0a0f30212caf58ecb5e5a9cfefe767e1ee2c4e
shin <cu7898@gmail.com>
2020-07-30 08:38:40 +0900

    TIL 불변

---
+++ core_javascript/Unit01/Chapter04/1-9.js
@@ -0,0 +1,17 @@
+var a = 10;
+var b = a;
+var obj1 = {
+    c: 10,
+    d: 'ddd'
+};
+var obj2 = obj1;
+
+b = 15;
+obj2 = {
+    c: 20,
+    d: 'ddd'
+};
+
+console.log(obj1 === obj2);
+console.log(obj1.c);
+console.log(obj2.c);
