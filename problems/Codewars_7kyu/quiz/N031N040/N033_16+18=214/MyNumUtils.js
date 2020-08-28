218f77a677edba07865936df45d92cd30fdf256b
shin <cu7898@gmail.com>
2020-08-28 09:36:32 +0900

    Codewars: 7kyu 16+18=214
    https://www.codewars.com/kata/5effa412233ac3002a9e471d/train/javascript

---
+++ problems/Codewars_7kyu/quiz/N031N040/N033_16+18=214/MyNumUtils.js
@@ -0,0 +1,13 @@
+class MyNumUtils {
+    static sillyAdd(num1, num2) {
+        return num1 + num2;
+    }
+
+    static devideDegit(num) {
+        const stringNum = num.toString();
+        const charDegit = stringNum.split('');
+        return charDegit.map(Number);
+    }
+}
+
+module.exports = MyNumUtils;
---
+++ problems/Codewars_7kyu/quiz/N031N040/N033_16+18=214/MyNumUtils.test.js
@@ -0,0 +1,17 @@
+const myNumUtils = require('./MyNumUtils');
+
+describe('sillyAdd', () => {
+    describe('with 1, 1', () => {
+        it('return 2', () => {
+            expect(myNumUtils.sillyAdd(1, 1)).toEqual(2);
+        });
+    });
+
+    describe('with 12, 19', () => {
+        it('return 211', () => {
+            console.log(myNumUtils.devideDegit(12));
+            
+            expect(myNumUtils.sillyAdd(12, 19)).toEqual(211);
+        });
+    });
+});
