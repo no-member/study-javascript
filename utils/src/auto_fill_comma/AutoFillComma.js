3a816b205381a8a992303bfe16f53f45b30a2fa5
Shin <cu7898@gmail.com>
2020-09-03 19:53:35 +0900

    Feat: 3자리마다 ,를 찍는 로직의 완성

--- utils/src/auto_fill_comma/AutoFillComma.js
+++ utils/src/auto_fill_comma/AutoFillComma.js
@@ -1,5 +1,17 @@
-const autoFillComma = function(stringNumber) {
-    return stringNumber;
+const autoFillCommaWithString = function(stringNumber) {
+    const removeCommaNumber = stringNumber.replace(/\,/g, '');
+
+    const regexPoint = RegExp(/\./);
+    if (regexPoint.test(removeCommaNumber)){
+        return removeCommaNumber.replace(/\B(?=(?=\d*\.)(\d{3})+(?!\d))/g, ',');
+    }
+    
+    // .을 생각하지 않을 경우 아래의 로직으로 만으로 충분하다.
+    return removeCommaNumber.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
-export { autoFillComma };
+const autoFillCommaWithNumber = function(number) {
+    return number.toLocaleString();
+};
+
+export { autoFillCommaWithString, autoFillCommaWithNumber };
--- utils/test/AutoFillComma.test.js
+++ utils/test/AutoFillComma.test.js
@@ -1,16 +1,60 @@
-import { autoFillComma } from '../src/auto_fill_comma/AutoFillComma.js'
+import { autoFillCommaWithString, autoFillCommaWithNumber } from '../src/auto_fill_comma/AutoFillComma.js'
 import { expect } from 'chai';
 
 describe('autoFillComma', () => {
     context('with 123', () => {
         it('return 123', () => {
-            expect(autoFillComma('123')).to.equal('123');
+            expect(autoFillCommaWithString('123')).to.equal('123');
         });
     });
 
     context('with 1234', () => {
         it('return 1,234', () => {
-            expect(autoFillComma('1234')).to.equal('1,234');
+            expect(autoFillCommaWithString('1234')).to.equal('1,234');
+        });
+    });
+
+    context('with 123456789', () => {
+        it('return 123,456,789', () => {
+            expect(autoFillCommaWithString('123456789')).to.equal('123,456,789');
+        });
+    });
+    
+    context('with 12345.6789', () => {
+        it('return 12,345.6789', () => {
+            expect(autoFillCommaWithString('12345.6789')).to.equal('12,345.6789');
+        });
+    });
+
+    context('with 12,345,6789', () => {
+        it('return 123,456,789', () => {
+            expect(autoFillCommaWithString('12,345,6789')).to.equal('123,456,789');
+        });
+    });
+    
+    context('with 123,456,789', () => {
+        it('return 123,456,789', () => {
+            expect(autoFillCommaWithString('123,456,789')).to.equal('123,456,789');
+        });
+    });
+});
+
+describe('autoFillCommaWithNumber', () => {
+    context('with 123', () => {
+        it('return 123', () => {
+            expect(autoFillCommaWithNumber(123)).to.equal('123');
+        });
+    });
+
+    context('with 1234', () => {
+        it('return 1,234', () => {
+            expect(autoFillCommaWithNumber(1234)).to.equal('1,234');
+        });
+    });
+
+    context('with 123456789', () => {
+        it('return 123,456,789', () => {
+            expect(autoFillCommaWithNumber(123456789)).to.equal('123,456,789');
         });
     });
 });
