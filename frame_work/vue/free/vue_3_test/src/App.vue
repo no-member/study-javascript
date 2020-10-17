74c8c960b0a4b35eed4ba3db0a98e126cc3b31fb
Shin <cu7898@gmail.com>
2020-10-17 10:27:30 +0900

    Apply .gitignore

---
+++ frame_work/vue/free/vue_3_test/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <hello-vue></hello-vue>
+  </div>
+</template>
+
+<script>
+import HelloVue from "@/components/HelloVue";
+
+export default {
+  components: {HelloVue}
+}
+</script>
+
+<style>
+</style>
