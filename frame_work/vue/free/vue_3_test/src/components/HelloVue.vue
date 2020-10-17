439ff1d37605e5eafed2442a38addd0d8c96a82a
Shin <cu7898@gmail.com>
2020-10-17 10:26:11 +0900

    Study: Vue3 공부

--- frame_work/vue/free/vue_3_test/src/components/HelloVue.vue
+++ frame_work/vue/free/vue_3_test/src/components/HelloVue.vue
@@ -1,13 +1,36 @@
 <template>
-$END$
+  <div id="hello-vue" class="demo">
+    {{ message }}
+  </div>
 </template>
 
 <script>
+import { Vue } from 'vue'
+
 export default {
 name: "HelloVue"
 }
+
+const HelloVueApp = {
+  data() {
+    return {
+      message: 'Hello Vue!!'
+    }
+  }
+}
+
+Vue.createApp({HelloVueApp}).mount('#hello-vue')
 </script>
 
 <style scoped>
-
+.demo {
+  font-family: sans-serif;
+  border: 1px solid #eee;
+  border-radius: 2px;
+  padding: 20px 30px;
+  margin-top: 1em;
+  margin-bottom: 40px;
+  user-select: none;
+  overflow-x: auto;
+}
 </style>
