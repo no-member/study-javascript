32dcd6edd032a4b201d4f3ae8a857a38a9c65039
Shin <cu7898@gmail.com>
2020-10-17 10:26:36 +0900

    Settings: gitignore

--- books/EthanBrown_Learning_JavaScript/.eslintrc.js
+++ books/EthanBrown_Learning_JavaScript/.eslintrc.js
@@ -1,20 +1,13 @@
 module.exports = {
     "env": {
-        "es6": true,
-        "node": true
+        "browser": true,
+        "es2021": true
     },
     "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
     "parserOptions": {
-        "ecmaVersion": 11,
+        "ecmaVersion": 12,
         "sourceType": "module"
     },
     "rules": {
-        "comma-dangle": [
-            2
-        ]
     }
 };
