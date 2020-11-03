88882e7d2f72fe6a9fcb0ec49edd5278d326cca5
S <cu7898@gmail.com>
2020-11-03 11:43:40 +0900

    Problem: summing_a_numbers_digits

--- problems/Codewars_7kyu/quiz/N031N040/n038_count_the_digit/summing_a_numbers_digits.js
+++ problems/Codewars_7kyu/quiz/N031N040/n038_count_the_digit/summing_a_numbers_digits.js
@@ -1,3 +1,7 @@
+/**
+ *
+ * @param number {number}
+ */
 const sumDigits = (number) => {
   return toDigitsArray(toAbs(number))
       .reduce(function(accumulator, current) {
@@ -5,10 +9,18 @@ const sumDigits = (number) => {
       });
 };
 
+/**
+ * 숫자의 절댓값을 구하는 함수
+ * @param number {number} 어느 숫자든 관계 없이 넣을 수  있음
+ */
 const toAbs = function(number) {
   return Math.abs(number);
 };
 
+/**
+ *
+ * @param number {number}
+ */
 const toDigitsArray = function(number) {
   return number.toString().split('')
       .map((stringDigit) => parseInt(stringDigit));
