a9d3aa030a742463d74d922097def59f7203ecb4
Shin <cu7898@gmail.com>
2020-11-29 23:46:37 +0900

    Test: 해당하는 값이 없는 경우 빈 배열을 반환 할 것

--- problems/Codewars_7kyu/quiz/N041N050/n041_no_oddities_here/number_utils.test.js
+++ problems/Codewars_7kyu/quiz/N041N050/n041_no_oddities_here/number_utils.test.js
@@ -9,5 +9,11 @@ describe('number_utils', () => {
                 expect(selectOdd([0, 1, 2])).to.eql([0, 2]);
             });
         });
+
+        context('[1, 3]를 입력 받았을 경우', () => {
+            it('Return [](빈 배열)을 반환 할 것', () => {
+                expect(selectOdd([1, 3])).to.eql([]);
+            });
+        });
     });
 })
