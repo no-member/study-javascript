113065b632c6176fd6bffed57502cc4694f9067a
Shin <cu7898@gmail.com>
2020-11-29 23:20:14 +0900

    Refactor: 한글을 사용해서 표현하도록 변경

--- problems/Codewars_7kyu/quiz/N041N050/n041_no_oddities_here/number_utils.test.js
+++ problems/Codewars_7kyu/quiz/N041N050/n041_no_oddities_here/number_utils.test.js
@@ -3,8 +3,8 @@ const expect = chai.expect;
 const selectOdd = require('./number_utils');
 
 describe('select_odd', () => {
-    context('With ([0, 1, 2])', () => {
-        it('Return [0, 2]', () => {
+    context('[0, 1, 2]를 입력 받았을 경우', () => {
+        it('Return [0, 2]를 반환 할 것', () => {
             expect(selectOdd([0, 1, 2])).to.eql([0, 2]);
         });
     });
