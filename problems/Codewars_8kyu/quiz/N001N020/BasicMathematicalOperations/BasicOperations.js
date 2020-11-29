2db531c1538fbb9033f2320da4a4bfeba403a630
S <cu7898@gmail.com>
2020-11-30 08:45:18 +0900

    Refactor: jsdoc 추가

--- problems/Codewars_8kyu/quiz/N001N020/BasicMathematicalOperations/BasicOperations.js
+++ problems/Codewars_8kyu/quiz/N001N020/BasicMathematicalOperations/BasicOperations.js
@@ -1,3 +1,10 @@
+/**
+ * 기본적인 계산을 해주는 함수
+ * @param {string} operation - '+', '-', '*', '/' 이외를 입력 받은 경우 0을 반환
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
 const basicOperation = function (operation, num1, num2) {
     switch (operation) {
         case '+':
--- problems/Codewars_8kyu/quiz/N001N020/CenturyFromYear/CenturyFromYear.js
+++ problems/Codewars_8kyu/quiz/N001N020/CenturyFromYear/CenturyFromYear.js
@@ -1,3 +1,8 @@
+/**
+ * 연도를 입력 받아 세기를 반환하는 함수
+ * @param {number} years
+ * @return {number} century
+ */
 const yearToCentury = function (years) {
     return Math.ceil(years / 100);
 };
