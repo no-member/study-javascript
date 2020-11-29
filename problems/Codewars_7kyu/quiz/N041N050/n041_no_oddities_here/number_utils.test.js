de5ac7c0204ecfaa5881be0291373f91a675ed1b
Shin <cu7898@gmail.com>
2020-11-29 23:45:07 +0900

    Refactor: 전체 테스트를 intellij에서 더욱 편하게 할 수 있도록 변경

--- problems/Codewars_7kyu/quiz/N041N050/n041_no_oddities_here/number_utils.test.js
+++ problems/Codewars_7kyu/quiz/N041N050/n041_no_oddities_here/number_utils.test.js
@@ -2,10 +2,12 @@ const chai = require('chai');
 const expect = chai.expect;
 const selectOdd = require('./number_utils');
 
-describe('select_odd', () => {
-    context('[0, 1, 2]를 입력 받았을 경우', () => {
-        it('Return [0, 2]를 반환 할 것', () => {
-            expect(selectOdd([0, 1, 2])).to.eql([0, 2]);
+describe('number_utils', () => {
+    describe('#select_odd()', () => {
+        context('[0, 1, 2]를 입력 받았을 경우', () => {
+            it('Return [0, 2]를 반환 할 것', () => {
+                expect(selectOdd([0, 1, 2])).to.eql([0, 2]);
+            });
         });
     });
-});
+})
