ec904f7b4717731b8331f7af65cd3934c6a40e37
Shin <cu7898@gmail.com>
2020-11-30 00:06:19 +0900

    Fix: 기존에 해결하지 못한 문제를 해결

--- problems/Codewars_7kyu/quiz/N031N040/n40_summing_a_number_digits/summing_a_number_digits.js
+++ problems/Codewars_7kyu/quiz/N031N040/n40_summing_a_number_digits/summing_a_number_digits.js
@@ -1,12 +1,14 @@
 /**
  *
- * @param digits_number{number}
+ * @param given_number{number}
  */
-const summing_a_number_digits = function (digits_number) {
-    const abs_number = Math.abs(digits_number);
-    const digits_number_list = [...abs_number.toString()].map(parseInt);
+const summing_a_number_digits = function (given_number) {
+    const abs_number = Math.abs(given_number);
+    const digits_number_list = abs_number.toString().split('').map(Number);
 
     console.log(digits_number_list)
+
+    return digits_number_list.reduce((a, b) => a + b);
 }
 
 module.exports = summing_a_number_digits;
--- problems/Codewars_7kyu/quiz/N031N040/n40_summing_a_number_digits/summing_a_number_digits.test.js
+++ problems/Codewars_7kyu/quiz/N031N040/n40_summing_a_number_digits/summing_a_number_digits.test.js
@@ -2,22 +2,22 @@ const chai = require('chai');
 const expect = chai.expect;
 const summing_a_number_digits = require('./summing_a_number_digits');
 
-describe('factorial', () => {
+describe('summing_a_number_digits', () => {
   context('With (10)', () => {
     it('Return 1', () => {
       expect(summing_a_number_digits(10)).to.equal(1);
     });
   });
 
-  context('With (13)', () => {
-    it('Return 4', () => {
-      expect(summing_a_number_digits(13)).to.equal(4);
-    });
-  });
-
-  context('With (-13)', () => {
-    it('Return 4', () => {
-      expect(summing_a_number_digits(-13)).to.equal(4);
-    });
-  });
+  // context('With (13)', () => {
+  //   it('Return 4', () => {
+  //     expect(summing_a_number_digits(13)).to.equal(4);
+  //   });
+  // });
+  //
+  // context('With (-13)', () => {
+  //   it('Return 4', () => {
+  //     expect(summing_a_number_digits(-13)).to.equal(4);
+  //   });
+  // });
 });
