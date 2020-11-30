8c46aaf1b1b28059b3e09802c9b3cc885f8550fd
S <cu7898@gmail.com>
2020-11-30 20:27:56 +0900

    Refactor: 기존 로직 가독성 향상
    변경 내용
    * 필요 없는 내용 삭제
    * jsdoc
    * BDD 구조로 테스트 변경

--- problems/Codewars_8kyu/quiz/N001N010/FindTheSmallestIntegerInTheArray/FindSmallest.js
+++ problems/Codewars_8kyu/quiz/N001N010/FindTheSmallestIntegerInTheArray/FindSmallest.js
@@ -1,9 +1,10 @@
-const findSmallest = function (array) {
-    // array.sort(function (a, b) {
-    //     return a - b;
-    // });
-    // return array[0];
-    return Math.min(...array);
+/**
+ * 주어진 숫자 배열에서 가장 작은 숫자를 반환해라
+ * @param {array} number_array
+ * @return {number} 주어진 배열에서 가장 작은 숫자
+ */
+const findSmallest = function (number_array) {
+    return Math.min(...number_array);
 };
 
 module.exports = findSmallest;
--- problems/Codewars_8kyu/quiz/N001N010/FindTheSmallestIntegerInTheArray/FindSmallet.test.js
+++ problems/Codewars_8kyu/quiz/N001N010/FindTheSmallestIntegerInTheArray/FindSmallet.test.js
@@ -1,17 +1,23 @@
 const findSmallest = require('./FindSmallest');
 
-test('findSmallest([1,2] toBe 1', () => {
-    expect(findSmallest([1, 2])).toBe(1);
-});
+describe('FindSmallest', () => {
+    describe('#findSmallest', () => {
+        context('([1, 2])는', () => {
+            it('1을 반환한다.', () => {
+                expect(findSmallest([1, 2])).toBe(1);
+            });
+        });
 
-test('findSmallest([5, 4, 2] toBe 2', () => {
-    expect(findSmallest([5, 4, 2])).toBe(2);
-});
+        context('([78, 56, 232, 12, 8])는', () => {
+            it('8을 반환한다.', () => {
+                expect(findSmallest([78, 56, 232, 12, 8])).toBe(8);
+            });
+        });
 
-test('findSmallest([78,56,232,12,8]) toBe 8', () => {
-    expect(findSmallest([78, 56, 232, 12, 8])).toBe(8);
-});
-
-test('findSmallest([34, -345, -1, 100]) toBe -345', () => {
-    expect(findSmallest([34, -345, -1, 100])).toBe(-345);
+        context('([34, -345, -1, 100])는', () => {
+            it('-345을 반환한다.', () => {
+                expect(findSmallest([34, -345, -1, 100])).toBe(-345);
+            });
+        });
+    });
 });
