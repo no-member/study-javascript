15b918a1a15aef4fe6167df11e8eebc7b2795758
S <cu7898@gmail.com>
2020-11-30 20:43:02 +0900

    Refactor: 기존 로직 가독성 향상
    변경 내용
    * 필요 없는 내용 삭제
    * jsdoc
    * BDD 구조로 테스트 변경

--- problems/Codewars_8kyu/quiz/N001N010/FunctionHelloWorld/HelloWorld.test.js
+++ problems/Codewars_8kyu/quiz/N001N010/FunctionHelloWorld/HelloWorld.test.js
@@ -1,9 +1,17 @@
 const greet = require('./HelloWorld');
 
-test('greet()는 "hello world!"를 반환한다.',() =>{
-    expect(greet()).toBe("hello world!");
-});
+describe('HelloWorld', () => {
+    describe('#greet', () => {
+        context('발동시키면', () => {
+            it('"hello world!"를 반환한다.', () => {
+                expect(greet()).toBe("hello world!");
+            });
+        });
 
-test('typeof greet는 "function"를 반환한다.',() =>{
-    expect(typeof greet).toBe("function");
+        context('typeof를 이용해 type 검사를 하면', () => {
+            it('"function"을 반환한다.', () => {
+                expect(typeof greet).toBe("function");
+            });
+        });
+    });
 });
