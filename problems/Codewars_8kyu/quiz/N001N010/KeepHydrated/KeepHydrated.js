be6a3c122da1576f3f355532201cd2da9f308407
S <cu7898@gmail.com>
2020-11-30 21:06:27 +0900

    Refactor: 기존 로직 가독성 향상
    변경 내용
    * jsdoc
    * BDD 구조로 테스트 변경

--- problems/Codewars_8kyu/quiz/N001N010/KeepHydrated/KeepHydrated.js
+++ problems/Codewars_8kyu/quiz/N001N010/KeepHydrated/KeepHydrated.js
@@ -1,3 +1,8 @@
+/**
+ * 시간을 입력 받으면 탈수 증상이 일어나지 않도록 필요한 최소한의 물의 양을 반환하는 함수
+ * @param {number} time
+ * @return {number} 필요한 물의 양
+ */
 const litres = function (time) {
     return Math.floor(time / 2);
 };
--- problems/Codewars_8kyu/quiz/N001N010/KeepHydrated/KeepHydrated.test.js
+++ problems/Codewars_8kyu/quiz/N001N010/KeepHydrated/KeepHydrated.test.js
@@ -1,21 +1,29 @@
 const litres = require('./KeepHydrated');
 
-test('keepHydrated(2)은 1를 반환함',() =>{
-    expect(litres(2)).toBe(1);
-});
+describe('KeepHydrated', () => {
+    describe('#litres', () => {
+        context('(2)은', () => {
+            it('keepHydrated 1를 반환함', () => {
+                expect(litres(2)).toBe(1);
+            });
+        });
 
-test('keepHydrated(1.4)은 0를 반환함',() =>{
-    expect(litres(1.4)).toBe(0);
-});
+        context('(1.4)은', () => {
+            it('keepHydrated(1.4)은 0를 반환함', () => {
+                expect(litres(1.4)).toBe(0);
+            });
+        });
 
-test('keepHydrated(12.3)은 6를 반환함',() =>{
-    expect(litres(12.3)).toBe(6);
-});
+        context('(11.8)은', () => {
+            it('5를 반환함', () => {
+                expect(litres(11.8)).toBe(5);
+            });
+        });
 
-test('keepHydrated(11.8)은 5를 반환함',() =>{
-    expect(litres(11.8)).toBe(5);
-});
-
-test('keepHydrated(1787)은 893를 반환함',() =>{
-    expect(litres(1787)).toBe(893);
+        context('(1787)은', () => {
+            it('893를 반환함', () => {
+                expect(litres(1787)).toBe(893);
+            });
+        });
+    });
 });
