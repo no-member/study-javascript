d69390b2cf35d0138c3cdc520d3f602b74978bcc
S <cu7898@gmail.com>
2020-12-02 21:21:07 +0900

    Feat: private 도입
    기존 코드 : 각종 값을 외부에서 변경 가능
    변경 코드 : private 도입을 통해 외부에서 값 변경 불능

--- problems/Codewars_7kyu/oop/N041N050/n042_count_the_divisors_of_a_number/NumberUtils.js
+++ problems/Codewars_7kyu/oop/N041N050/n042_count_the_divisors_of_a_number/NumberUtils.js
@@ -1,10 +1,14 @@
 /** 1부터 자기 자신까지의 값에서 나누어 떨어지는 값의 개수를 구하는 클래스 */
 class NumberUtils {
+  #targetNumber;
+  #targetNumberRange;
+  #divisorsCount;
+
   /**
    * @param {number} targetNumber
    */
   constructor(targetNumber) {
-    this.targetNumber = targetNumber;
+    this.#targetNumber = targetNumber;
     this.initCountAndRange();
   }
 
@@ -13,23 +17,23 @@ class NumberUtils {
    */
   getDivisorsCount = () => {
     this.initCountAndRange();
-    for (let i = 0; i < this.targetNumber; i++) {
-      this.targetNumberRange.push(i + 1);
+    for (let i = 0; i < this.#targetNumber; i++) {
+      this.#targetNumberRange.push(i + 1);
     }
 
-    for (let i = 0; i < this.targetNumber; i++) {
-      if (this.targetNumber % this.targetNumberRange[i] === 0) {
-        this.divisorsCount += 1;
+    for (let i = 0; i < this.#targetNumber; i++) {
+      if (this.#targetNumber % this.#targetNumberRange[i] === 0) {
+        this.#divisorsCount += 1;
       }
     }
 
-    return this.divisorsCount;
+    return this.#divisorsCount;
   };
 
-  initCountAndRange = () => {
-    this.targetNumberRange = [];
-    this.divisorsCount = 0;
-  }
+  initCountAndRange() {
+    this.#targetNumberRange = [];
+    this.#divisorsCount = 0;
+  };
 }
 
 module.exports = NumberUtils;
