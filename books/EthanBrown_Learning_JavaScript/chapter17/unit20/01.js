4163d8d810a14fcfdf009b29d92990af00b83371
Shin <cu7898@gmail.com>
2020-12-13 22:51:19 +0900

    Feat: 러닝 자바스크립트 - 정규식
    RegEx 객체를 이용한 동적 정규식 생성

---
+++ books/EthanBrown_Learning_JavaScript/chapter17/unit20/01.js
@@ -0,0 +1,8 @@
+const users = ['mary', 'nick', 'arthur', 'sam', 'yvette'];
+const text = 'User @arthur started the backup and 15:15, ' +
+  'and @nick and @yvette restored it at 18:35.';
+
+const userRegex = new RegExp(`@(?:${users.join('|')})\\b`, 'g');
+
+console.log(text.match(userRegex));
+
