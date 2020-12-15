9a14b5d36d52ed9030edf9a3187e2eb999f66937
Shin <cu7898@gmail.com>
2020-12-16 00:00:22 +0900

    [Feat] 마운트 요소 외의 이벤트 조작
    스크롤 이벤트 추출하기

---
+++ frame_work/vue/books/vue_for_cat/chapter03/unit15/section01/main.js
@@ -0,0 +1,24 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        scrollY: 0,
+        timer: null
+    },
+    created: function () {
+        window.addEventListener('scroll', this.handleScroll)
+    },
+    beforeDestroy: function () {
+        window.removeEventListener('scroll', this.handleScroll)
+    },
+    methods: {
+        handleScroll() {
+            if (this.timer === null) {
+                this.timer = setTimeout(function () {
+                    this.scrollY = window.scrollY;
+                    clearTimeout(this.timer);
+                    this.timer = null
+                }.bind(this), 200);
+            }
+        }
+    }
+})
